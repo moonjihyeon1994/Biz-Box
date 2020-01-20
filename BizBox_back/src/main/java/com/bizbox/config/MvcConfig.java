62c496dd01af0f5c70411239bc35cc05cd284ca3
문지현 <puppy613@hanmail.net>
2020-01-20 13:30:35 +0900

    init
    Former-commit-id: de1b65aeabd3fcf665e8f4b28da52926f47bde61

---
+++ BizBox_back/src/main/java/com/bizbox/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.bizbox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // 환경 설정 파일임을 지정, 스프링이 내부적으로 proxy 생성 --> 싱글턴으로 빈 관리
+public class MvcConfig implements WebMvcConfigurer { // 이녀석을 상속받으면 override로 메서드들을 사용할 수 있다.
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
