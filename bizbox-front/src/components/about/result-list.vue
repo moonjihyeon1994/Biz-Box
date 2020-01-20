9a749a5482fe1d4411f48ec075e3ba7325b4424e
heecheol <heecheol1508@gmail.com>
2020-01-20 17:51:42 +0900

    created result-list file  - display dummy data about the place
    Former-commit-id: 0c3036d874d90991c2b96009ac1d63822760dcf3 [formerly b1c47ad0fc860dd54d088109eb7438989973b6ce]
    Former-commit-id: d170b5477150c14fa6e634845505b34041685bef

---
+++ bizbox-front/src/components/about/result-list.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="types.length">
+    <ul>
+      <li v-for="item in types" :key="item.name">
+        {{ item.name }} {{item.count}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      types: [{ 'name': 'pizza', 'count': 30 }, { 'name': 'korean', 'count': 100 }, { 'name': 'sushi', 'count': 50 }]
+    }
+  }
+}
+</script>
--- bizbox-front/src/router/index.js
+++ bizbox-front/src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '@/components/about/About.vue')
+  },
+  {
+    path: '/result-list',
+    name: 'result-list',
+    component: () => import('@/components/about/result-list.vue')
   }
 ]
 
