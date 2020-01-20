1976f8422d791bbd9a7ed4cd637c4ecf679e46b4
문지현 <puppy613@hanmail.net>
2020-01-20 13:30:35 +0900

    init
    Former-commit-id: 83bcbb3401357236c86565bf59630b39ae61f17b

---
+++ BizBox_back/src/main/java/com/bizbox/BizBoxApplication.java
@@ -0,0 +1,13 @@
+package com.bizbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizBoxApplication.class, args);
+	}
+
+}
