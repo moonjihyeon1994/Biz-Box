b5289c389f980c258250b6ea77608a479053d187
osw <tmddhks4717@naver.com>
2020-01-20 17:53:48 +0900

    20-01-20 nothing osw
    Former-commit-id: 6460ebf000a9f554367222afad6d1102f682af83 [formerly 746d70186e5fec690e4f6771532fc538a5bd7c69]
    Former-commit-id: 4b6f51556016feff2b6c5630499a10ff082779c0

--- BizBox_back/src/main/java/com/bizbox/Service/boardService.java
+++ BizBox_back/src/main/java/com/bizbox/Service/boardService.java
@@ -1,14 +1,14 @@
-package com.ssafy.model.service;
-
-import java.util.List;
-
-import com.ssafy.model.dto.Board;
-
-public interface boardService {
-	public List<Board> findAllNotice();
-	public boolean updateBoard(Board board);
-	public boolean addBoard(Board board);
-	public boolean deleteboard(int bidx);
-	public Board detailNotice(int bidx);
-	public Board detailincrease(int bidx);
-}
+//package com.ssafy.model.service;
+//
+//import java.util.List;
+//
+//import com.ssafy.model.dto.Board;
+//
+//public interface boardService {
+//	public List<Board> findAllNotice();
+//	public boolean updateBoard(Board board);
+//	public boolean addBoard(Board board);
+//	public boolean deleteboard(int bidx);
+//	public Board detailNotice(int bidx);
+//	public Board detailincrease(int bidx);
+//}
