14063a7baaf0d1155780f8c1013d3a0a387d5576
heecheol <heecheol1508@gmail.com>
2020-01-21 17:02:08 +0900

    About-resultList with icons
    Former-commit-id: b65d67e63aef6d88040f2a9ddbfc20f70a9e5ff6 [formerly ce146696e54ea08e20a8743e2432e97697f55322]
    Former-commit-id: 29e857ccb2281b85cf85bb2ff2e44a18d6e3b149

--- bizbox-front/src/components/about/About.vue
+++ bizbox-front/src/components/about/About.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <resultList></resultList>
+    <v-row>
+      <resultList></resultList>
+    </v-row>
   </div>
 </template>
 <script>
