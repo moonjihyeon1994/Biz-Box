8ac3fd5a0c80a20cd316ce7b264ccad3ba54814f
문요한 <munyohan@naver.com>
2020-01-21 17:48:44 +0900

    [front 문요한] Graph 및 Chart 업로드
    Former-commit-id: 4d786f61490ec8a96fdfcead0414c3f39efb6bd3 [formerly 5e68d3ee9c53397daec1fc514ff43608e9bf7db7]
    Former-commit-id: db2a0794c87659070aa5921e640453ee7f1eed10

--- bizbox-front/src/components/home/Graph.vue
+++ bizbox-front/src/components/home/Graph.vue
@@ -2,12 +2,27 @@
   <div>
     <h1>문요한의 차트 및 그래프 테스트 컴포넌트</h1>
     <hr />
+
+    <div id="columnchart_values" style="width: 900px; height: 300px;"></div>
   </div>
 </template>
 
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script>
-export default {};
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  mounted() {
+    this.renderChart(data, options);
+  }
+};
 </script>
 
+
+
 <style>
+#columnchart_values {
+  border: 1px solid black;
+}
 </style>
