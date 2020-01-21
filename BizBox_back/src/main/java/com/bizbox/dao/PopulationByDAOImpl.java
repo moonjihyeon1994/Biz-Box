042f026fe1a13d4a3727e27c7e8e7311c88af087
Eumyounghyun <opwer032@naver.com>
2020-01-21 11:57:54 +0900

    20-01-21 형식 맞춤 / 유동인구, 거주인구 테스트 완료
    Former-commit-id: d72a1b25b5c873b3791270b531e4a9d1b1ba6f87 [formerly 65c9a50306a8d5619340f5351b08f375087abed0]
    Former-commit-id: 0d7368f507080a195b09dada7a4545ce2f7217e3

---
+++ BizBox_back/src/main/java/com/bizbox/dao/PopulationByDAOImpl.java
@@ -0,0 +1,29 @@
+package com.bizbox.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bizbox.vo.PopulationByLocation;
+import com.bizbox.vo.PopulationByTime;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+public class PopulationByDAOImpl implements PopulationByDAO {
+
+	private final String name ="com.bizbox.mapper.populationByMapper.";
+	
+	@Autowired
+	SqlSession session;
+
+	@Override
+	public PopulationByTime populationByTime(String address) throws Exception{
+		return session.selectOne(name+"selectByTimeAdress",address);
+	}
+	@Override
+	public PopulationByLocation populationByLocation(String address) throws Exception {
+		return session.selectOne(name+"populationByLocation",address);
+	}
+}
--- BizBox_back/src/main/java/com/bizbox/vo/PopulationByTime.java
+++ BizBox_back/src/main/java/com/bizbox/vo/PopulationByTime.java
@@ -1,10 +1,13 @@
 package com.bizbox.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PopulationByTime {
-
-	
-	
 		
 		private String a;
 		private String b;
@@ -28,140 +31,5 @@ public class PopulationByTime {
 		private String t;
 		private String u;
 		private String v;
-		public String getA() {
-			return a;
-		}
-		public void setA(String a) {
-			this.a = a;
-		}
-		public String getB() {
-			return b;
-		}
-		public void setB(String b) {
-			this.b = b;
-		}
-		public String getC() {
-			return c;
-		}
-		public void setC(String c) {
-			this.c = c;
-		}
-		public String getD() {
-			return d;
-		}
-		public void setD(String d) {
-			this.d = d;
-		}
-		public String getE() {
-			return e;
-		}
-		public void setE(String e) {
-			this.e = e;
-		}
-		public String getF() {
-			return f;
-		}
-		public void setF(String f) {
-			this.f = f;
-		}
-		public String getG() {
-			return g;
-		}
-		public void setG(String g) {
-			this.g = g;
-		}
-		public String getH() {
-			return h;
-		}
-		public void setH(String h) {
-			this.h = h;
-		}
-		public String getI() {
-			return i;
-		}
-		public void setI(String i) {
-			this.i = i;
-		}
-		public String getJ() {
-			return j;
-		}
-		public void setJ(String j) {
-			this.j = j;
-		}
-		public String getK() {
-			return k;
-		}
-		public void setK(String k) {
-			this.k = k;
-		}
-		public String getL() {
-			return l;
-		}
-		public void setL(String l) {
-			this.l = l;
-		}
-		public String getM() {
-			return m;
-		}
-		public void setM(String m) {
-			this.m = m;
-		}
-		public String getN() {
-			return n;
-		}
-		public void setN(String n) {
-			this.n = n;
-		}
-		public String getO() {
-			return o;
-		}
-		public void setO(String o) {
-			this.o = o;
-		}
-		public String getP() {
-			return p;
-		}
-		public void setP(String p) {
-			this.p = p;
-		}
-		public String getQ() {
-			return q;
-		}
-		public void setQ(String q) {
-			this.q = q;
-		}
-		public String getR() {
-			return r;
-		}
-		public void setR(String r) {
-			this.r = r;
-		}
-		public String getS() {
-			return s;
-		}
-		public void setS(String s) {
-			this.s = s;
-		}
-		public String getT() {
-			return t;
-		}
-		public void setT(String t) {
-			this.t = t;
-		}
-		public String getU() {
-			return u;
-		}
-		public void setU(String u) {
-			this.u = u;
-		}
-		public String getV() {
-			return v;
-		}
-		public void setV(String v) {
-			this.v = v;
-		}
-		
-		
-	
 
 }
