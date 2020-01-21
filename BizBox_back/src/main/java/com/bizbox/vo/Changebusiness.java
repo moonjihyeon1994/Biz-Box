5d5691e9574bcfd6a00a2b30f40f739b2781ce4f
osw <tmddhks4717@naver.com>
2020-01-21 15:39:19 +0900

    porm.xml
    Former-commit-id: fbc7a6abf1a08f000834ad329243e02fcc992872 [formerly a6b66f6e52f884093a663fb332427f44b5f60971]
    Former-commit-id: 2d504becc2242d5da370e7ea0971f3d018a308da

---
+++ BizBox_back/src/main/java/com/bizbox/vo/Changebusiness.java
@@ -0,0 +1,23 @@
+package com.bizbox.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Changebusiness {
+	
+	String a;
+	String b;
+	String c;
+	String d;
+	String e;
+	String f;
+	String g;
+	String h;
+	String i;
+	String j;
+
+}
