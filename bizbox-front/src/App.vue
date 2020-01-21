04f5bbb75318ad3fe93d57ab549d43d3cbea9d7f
heecheol <heecheol1508@gmail.com>
2020-01-21 14:53:00 +0900

    add vuetify
    Former-commit-id: 2fc3806438cd70cbd4e2d079bea4bde95be8c4d2 [formerly 5de8a2eafa85951a398c77b76d5a45353c06ab32]
    Former-commit-id: b0686121c92d261927b1c86373b865168b0521be

---
+++ bizbox-front/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/" class="link1">Home</router-link>|
+      <router-link to="/about" class="link1">About</router-link>|
+      <router-link to="/result" class="link1">Result</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #011627;
+  background-color: #ECE6CD;
+}
+#nav {
+  padding: 30px;
+  background: #3A3F58;
+}
+#nav a {
+  font-weight: bold;
+  color: #FFFFFF;
+}
+#nav a.router-link-exact-active {
+  color: #42B983;
+}
+.link-1 {
+  transition: 0.3s ease;
+  background: #3FA46A;
+  color: #FFFFFF;
+  font-size: 20px;
+  text-decoration: none;
+  border-top: 4px solid #3FA46A;
+  border-bottom: 4px solid #3FA46A;
+  padding: 20px 0;
+  margin: 0 20px;
+}
+.link-1:hover {
+  border-top: 4px solid #FFFFFF;
+  border-bottom: 4px solid #FFFFFF;
+  padding: 6px 0;
+}
+</style>
--- bizbox-front/src/components/about/About.vue
+++ bizbox-front/src/components/about/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <resultList></resultList>
   </div>
 </template>
+<script>
+import resultList from '@/components/about/resultList.vue'
+export default {
+  components: {
+    resultList
+  }
+}
+</script>
