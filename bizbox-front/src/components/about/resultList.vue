04cce8cb6a8213737e407514a2968a0a586374d7
heecheol <heecheol1508@gmail.com>
2020-01-21 14:53:00 +0900

    add vuetify
    Former-commit-id: 560618f45878fe8bd6e866ac2cc9f77282019fde [formerly 7dc368f618f3c3a802bf50cfe3d289af46b468aa]
    Former-commit-id: b9349dd0398ce66267ee8e92a89da5d930e56d2c

---
+++ bizbox-front/src/components/about/resultList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list>
+    <v-list-item>
+      <v-icon>mdi-home</v-icon>
+    </v-list-item>
+    <v-icon>mdi-event</v-icon>
+  </v-list>
+</template>
+
+<script>
+// axios.get()
+
+export default {
+  name: 'resultList',
+  data () {
+    return {
+      info: {
+        'g': '521112',
+        'h': '307888',
+        'i': '213230',
+        'j': '27767',
+        'k': '93477'
+      }
+    }
+  }
+}
+</script>
