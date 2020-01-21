7b9822347434dca13353866698f9592e14a2bee1
osw <tmddhks4717@naver.com>
2020-01-21 10:18:33 +0900

    20-01-21 population-osw
    Former-commit-id: 66abd5f61721f31c516dbca74c4d10dd82e6bf32 [formerly 66238ea01e85ca0322bfbc9c7fdcd523227bbdb4]
    Former-commit-id: 98e09b0045d672301b9606ae941512fa506a2084

---
+++ BizBox_back/src/main/java/com/bizbox/dao/populationBtDAO.java
@@ -0,0 +1,11 @@
+package com.bizbox.dao;
+
+import java.util.List;
+
+import com.bizbox.vo.*;
+
+public interface populationBtDAO {
+	
+	PopulationByTime findAllpopulation(String address);
+	
+}
---
+++ BizBox_back/src/main/java/com/bizbox/vo/PopulationByTime.java
@@ -0,0 +1,167 @@
+package com.bizbox.vo;
+
+
+public class PopulationByTime {
+
+	
+	
+		
+		private String a;
+		private String b;
+		private String c;
+		private String d;
+		private String e;
+		private String f;
+		private String g;
+		private String h;
+		private String i;
+		private String j;
+		private String k;
+		private String l;
+		private String m;
+		private String n;
+		private String o;
+		private String p;
+		private String q;
+		private String r;
+		private String s;
+		private String t;
+		private String u;
+		private String v;
+		public String getA() {
+			return a;
+		}
+		public void setA(String a) {
+			this.a = a;
+		}
+		public String getB() {
+			return b;
+		}
+		public void setB(String b) {
+			this.b = b;
+		}
+		public String getC() {
+			return c;
+		}
+		public void setC(String c) {
+			this.c = c;
+		}
+		public String getD() {
+			return d;
+		}
+		public void setD(String d) {
+			this.d = d;
+		}
+		public String getE() {
+			return e;
+		}
+		public void setE(String e) {
+			this.e = e;
+		}
+		public String getF() {
+			return f;
+		}
+		public void setF(String f) {
+			this.f = f;
+		}
+		public String getG() {
+			return g;
+		}
+		public void setG(String g) {
+			this.g = g;
+		}
+		public String getH() {
+			return h;
+		}
+		public void setH(String h) {
+			this.h = h;
+		}
+		public String getI() {
+			return i;
+		}
+		public void setI(String i) {
+			this.i = i;
+		}
+		public String getJ() {
+			return j;
+		}
+		public void setJ(String j) {
+			this.j = j;
+		}
+		public String getK() {
+			return k;
+		}
+		public void setK(String k) {
+			this.k = k;
+		}
+		public String getL() {
+			return l;
+		}
+		public void setL(String l) {
+			this.l = l;
+		}
+		public String getM() {
+			return m;
+		}
+		public void setM(String m) {
+			this.m = m;
+		}
+		public String getN() {
+			return n;
+		}
+		public void setN(String n) {
+			this.n = n;
+		}
+		public String getO() {
+			return o;
+		}
+		public void setO(String o) {
+			this.o = o;
+		}
+		public String getP() {
+			return p;
+		}
+		public void setP(String p) {
+			this.p = p;
+		}
+		public String getQ() {
+			return q;
+		}
+		public void setQ(String q) {
+			this.q = q;
+		}
+		public String getR() {
+			return r;
+		}
+		public void setR(String r) {
+			this.r = r;
+		}
+		public String getS() {
+			return s;
+		}
+		public void setS(String s) {
+			this.s = s;
+		}
+		public String getT() {
+			return t;
+		}
+		public void setT(String t) {
+			this.t = t;
+		}
+		public String getU() {
+			return u;
+		}
+		public void setU(String u) {
+			this.u = u;
+		}
+		public String getV() {
+			return v;
+		}
+		public void setV(String v) {
+			this.v = v;
+		}
+		
+		
+	
+
+}
