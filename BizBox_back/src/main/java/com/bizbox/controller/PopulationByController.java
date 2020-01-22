7e57c2ef6d325d0a163f87a6ac7ee3ab0f8c667c
문지현 <puppy613@hanmail.net>
2020-01-22 12:01:40 +0900

    something change
    Former-commit-id: f18d13cfdc985463aee436b953362cd14805c252 [formerly a0071f3f4e3636dc236494199eeab779f1a3e0ce]
    Former-commit-id: 650c9ce8e60e65d5d63b04c6b3b048c5ac8646f6

--- BizBox_back/src/main/java/com/bizbox/controller/PopulationByController.java
+++ BizBox_back/src/main/java/com/bizbox/controller/PopulationByController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bizbox.Service.PopulationByService;
+import com.bizbox.vo.PopulationByDong;
 import com.bizbox.vo.PopulationByLocation;
 import com.bizbox.vo.PopulationByTime;
 
