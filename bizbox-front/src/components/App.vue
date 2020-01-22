921af3fa4213c2942b565ca70ad79fbe9cd11f75
JaehyunKim94 <didwjd169@gmail.com>
2020-01-22 13:53:12 +0900

    CSS: Grid System added
    Former-commit-id: aca01717a798c1027e18308ca2ea564a6ff2ab55 [formerly 03218560d320c7c78131464ed583e7a8373093a9]
    Former-commit-id: 87e8c5830be6e50eddddccfc0b6f487e17dabad5

--- bizbox-front/src/components/App.vue
+++ bizbox-front/src/components/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/" class="link1">Home</router-link>
-      <router-link to="/about" class="link1">About</router-link>
-      <router-link to="/result" class="link1">Result</router-link>
+    <v-tabs>
+      <v-tab link to="/" class="link1">BizBox</v-tab>
+      <v-tab link to="/result" class="link1">Result</v-tab>
+      <v-tab link to="/about" class="link1">About</v-tab>
+    </v-tabs>
+    <div class="container">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
+<script>
+export default {
+  name: 'app'
+}
+</script>
 <style src="@/assets/css/reset.css"></style>
-<style src="@/components/common/common.css"></style>
 <style src="@/components/common/nav.css"></style>
+<style src="@/components/common/common.css"></style>
