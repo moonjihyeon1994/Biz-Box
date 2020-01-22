576a1744f8a506d7e97e9f1892dc91e77b62a0a0
문지현 <puppy613@hanmail.net>
2020-01-22 13:37:44 +0900

    조그만 수정
    Former-commit-id: 145bb41e80f041a48405c2bd1ecdf85f5a910a1a [formerly 8cefe9eade577be6d0fa1d159a782869d38a9071]
    Former-commit-id: 68488affe25973cf2bdf8a95e21aa3abdb872279

--- BizBox_back/src/main/java/com/bizbox/controller/StoreCountController.java
+++ BizBox_back/src/main/java/com/bizbox/controller/StoreCountController.java
@@ -6,26 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bizbox.apis.JusoApi;
-import com.bizbox.vo.PopulationByTime;
-import com.google.gson.JsonParser;
-import com.jhlabs.map.proj.Projection;
-import com.jhlabs.map.proj.ProjectionFactory;
 
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.awt.geom.Point2D;
-import java.io.BufferedReader;
-import java.io.IOException;
 
 @RestController
 @Slf4j
@@ -36,7 +22,6 @@ public class StoreCountController {
 	
 	@GetMapping("/storecount/{address}/{range}")
 	public ResponseEntity<Object> getStoreNumByCount(@PathVariable String address,@PathVariable String range){
-		PopulationByTime pbt;
 		try {
 			String num = api.getAddressByName(address);
 			String xy = api.getAddressByXY(num);
