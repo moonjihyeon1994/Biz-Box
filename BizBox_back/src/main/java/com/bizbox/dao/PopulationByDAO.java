804d2d61af4653795bf9a38f1a8ff1e4ef4ca5a1
Eumyounghyun <opwer032@naver.com>
2020-01-22 11:19:07 +0900

    20-01-22 시간별 생활인구 / 동검색
    Former-commit-id: c0f5f2c8a2f64ff61b384c2c9c6eb042760e1588 [formerly ff7a4046866d221da068b1e1c891567193177fe7]
    Former-commit-id: 1380714c63720627926be8cc78e25915ece65fc3

--- BizBox_back/src/main/java/com/bizbox/dao/PopulationByDAO.java
+++ BizBox_back/src/main/java/com/bizbox/dao/PopulationByDAO.java
@@ -1,5 +1,8 @@
 package com.bizbox.dao;
 
+import java.util.List;
+
+import com.bizbox.vo.PopulationByDong;
 import com.bizbox.vo.PopulationByLocation;
 import com.bizbox.vo.PopulationByTime;
 
@@ -7,5 +10,5 @@ public interface PopulationByDAO {
 	
 	PopulationByTime populationByTime(String address) throws Exception;
 	PopulationByLocation populationByLocation(String address) throws Exception;
-	
+	List<PopulationByDong> populationByDong(String address) throws Exception;
 }
---
+++ BizBox_back/src/main/java/com/bizbox/vo/PopulationByDong.java
@@ -0,0 +1,43 @@
+package com.bizbox.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PopulationByDong {
+	private String A;
+	private String B;
+	private String C;
+	private String D;
+	private String E;
+	private String F;
+	private String G;
+	private String H;
+	private String I;
+	private String J;
+	private String K;
+	private String L;
+	private String M;
+	private String N;
+	private String O;
+	private String P;
+	private String Q;
+	private String R;
+	private String S;
+	private String T;
+	private String U;
+	private String V;
+	private String W;
+	private String X;
+	private String Y;
+	private String Z;
+	private String AA;
+	private String AB;
+	private String AC;
+	private String AD;
+	private String AE;
+	private String AF;
+}
