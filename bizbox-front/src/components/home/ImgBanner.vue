17226803d44063915c242642138c1dddbcc28dab
JaehyunKim94 <didwjd169@gmail.com>
2020-01-23 14:43:48 +0900

    200123_Setting&Features_set prettier & select comp added
    Former-commit-id: f9de1fde49855bcfde77acb01279afe5e94639e5 [formerly ecb662a43c0403d5fb4f424fd75cfe186b7eb915]
    Former-commit-id: fb5aa0e134b8bd34496d8dccbf295fa1a9dd0f65

---
+++ bizbox-front/src/components/home/ImgBanner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="imgbner"></div>
+</template>
+
+<script>
+export default {
+  name: 'ImgBanner'
+}
+</script>
+
+<style scoped src="@/components/home/ImgBanner.css"></style>
