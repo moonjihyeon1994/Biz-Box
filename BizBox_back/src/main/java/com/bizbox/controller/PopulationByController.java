e4ab2f872213be57d33a26e18e7a1fde7ac43686
osw <tmddhks4717@naver.com>
2020-01-23 12:12:25 +0900

    20-01-23 데이터 리턴타입 변경 및 검색방식 변경및 에러처리 osw
    Former-commit-id: 23844daaa7eca9e910123cd3a615c073bbea4306 [formerly 6c3fb2018c560fd4de1981a2b08f30e73cf4ad8c]
    Former-commit-id: 118cb3dc4f73749e70448b44c47d430cbe281b24

--- BizBox_back/src/main/java/com/bizbox/controller/PopulationByController.java
+++ BizBox_back/src/main/java/com/bizbox/controller/PopulationByController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bizbox.Service.PopulationByService;
+import com.bizbox.apis.JusoApi;
 import com.bizbox.vo.PopulationByDong;
 import com.bizbox.vo.PopulationByLocation;
 import com.bizbox.vo.PopulationByTime;
@@ -24,30 +26,69 @@ import com.bizbox.vo.PopulationByTime;
 @RestController
 @RequestMapping("/population")
 public class PopulationByController {
-
+	private final int max=11715962;
+	private final int avgmax=5073881;
+	private final int avg=1568200;
+	private final int avgmin=783098;
+	private final int min=2004;
+	
 	@Autowired
 	PopulationByService service;
 	
 	@GetMapping("/getPopulationByTime/{address}")
 	public ResponseEntity<Object> getPopulationByTime(@PathVariable String address){
-		Map<Integer, PopulationByTime> map=new HashMap<Integer, PopulationByTime>();
 		PopulationByTime pbt;
 		int Point=0;
 		try {
+			JSONObject jsonObject = new JSONObject();
 			pbt = service.populationByTime(address);
-			return new ResponseEntity<Object>(pbt,HttpStatus.OK);
+			if(pbt==null) {
+				JusoApi api = new JusoApi();
+				String predoroname=api.getAddressByName(address).split(",")[11];
+				String[] string=predoroname.split(" ");
+				String doroname = string[2];
+				System.out.println(doroname);
+				int size=doroname.length();
+				for(int i=0; i<size-1; i++) {
+				pbt = service.populationByTime(doroname);
+				if(pbt!=null)break;
+				doroname=doroname.substring(0, size-1-i);
+				}
+			}
+			jsonObject.put("pbt", pbt);
+			jsonObject.put("point", Point);
+			if(pbt==null) {return new ResponseEntity<Object>("해당주소의 데이터가 존재하지 않습니다.",HttpStatus.NOT_FOUND);}
+			return new ResponseEntity<Object>(jsonObject,HttpStatus.OK);
 		} catch (Exception e) {
 			
 			e.printStackTrace();
-			return new ResponseEntity<Object>("error", HttpStatus.NOT_FOUND);
+			return new ResponseEntity<Object>("해당주소가 존재하지않습니다.", HttpStatus.NOT_FOUND);
 		}
 	}
 	@GetMapping("/getPopulationByLocation/{address}")
 	public ResponseEntity<Object> getPopulationByLocation(@PathVariable String address){
 		PopulationByLocation pbl;
+		int Point=0;
 		try {
+			JSONObject jsonObject = new JSONObject();
 			pbl = service.populationByLocation(address);
-			return new ResponseEntity<Object>(pbl,HttpStatus.OK);
+			if(pbl==null) {
+				JusoApi api = new JusoApi();
+				String predoroname=api.getAddressByName(address).split(",")[11];
+				String[] string=predoroname.split(" ");
+				String doroname = string[2];
+				System.out.println(doroname);
+				int size=doroname.length();
+				for(int i=0; i<size-1; i++) {
+					pbl = service.populationByLocation(doroname);
+				if(pbl!=null)break;
+				doroname=doroname.substring(0, size-1-i);
+				}
+			}
+			jsonObject.put("pbl", pbl);
+			jsonObject.put("point", Point);
+			if(pbl==null) {return new ResponseEntity<Object>("해당주소의 데이터가 존재하지 않습니다.",HttpStatus.NOT_FOUND);}
+			return new ResponseEntity<Object>(jsonObject,HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<Object>("error", HttpStatus.NOT_FOUND);
@@ -56,10 +97,14 @@ public class PopulationByController {
 	@GetMapping("/getPopulationByDong/{address}")
 	public ResponseEntity<Object> getPopulationByDong(@PathVariable String address){
 		List<PopulationByDong> pbd;
+		int Point=0;
 		try {
+			JSONObject jsonObject = new JSONObject();
 			pbd = service.populationByDong(address);
 			System.out.println(pbd);
-			return new ResponseEntity<Object>(pbd,HttpStatus.OK);
+			jsonObject.put("pbd", pbd);
+			jsonObject.put("point", Point);
+			return new ResponseEntity<Object>(jsonObject,HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<Object>("error", HttpStatus.NOT_FOUND);
