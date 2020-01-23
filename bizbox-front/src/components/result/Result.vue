3fabb336b9d1fea5be8754484711a150407d01f4
JaehyunKim94 <didwjd169@gmail.com>
2020-01-23 14:43:48 +0900

    200123_Setting&Features_set prettier & select comp added
    Former-commit-id: 3066e030c0c9e6c7d32e1464a74f8e4b45c8691f [formerly 56211524f07edc735c34aca5e72604e3d56a4529]
    Former-commit-id: 3949eee7ecae4b599171a0faa0220714c9f48f67

--- bizbox-front/src/components/result/Result.vue
+++ bizbox-front/src/components/result/Result.vue
@@ -1,20 +1,28 @@
 <template>
-<div class="row">
-  <div class="col-4">
-    <h1>col 3</h1>
+  <div class="row">
+    <div class="col-2"><Select :idx="items1.idx" :items="items1.item"></Select></div>
+    <div class="col-2"><Select :idx="items2.idx" :items="items2.item"></Select></div>
   </div>
-  <div class="col-9">
-    <h1>Hi this is result</h1>
-  </div>
-</div>
-
 </template>
 
 <script>
-export default {}
-</script>
-<style scoped>
-  .item1 {
-    grid-row: 1 / 6 / 1/ 2;
+import Select from '@/components/common/Select.vue'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      items1: {
+        idx: '시/도',
+        item: ['서울', '부산', '울산']
+      },
+      items2: {
+        idx: '구/군',
+        item: ['강남구', '강서구', '강북구']
+      }
+    }
+  },
+  components: {
+    Select
   }
-</style>
+}
+</script>
