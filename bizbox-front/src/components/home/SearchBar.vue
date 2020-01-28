8f0026becd9c9164301214311b833bf223974d6d
JaehyunKim94 <didwjd169@gmail.com>
2020-01-28 17:58:45 +0900

    200128_김재현_SearchBar updated
    Former-commit-id: 5cb6269df8e889c3ddb04daf5bb2ce1b709d3c28 [formerly 56668a7e8b09f4f95e9b108376bf39b9b7df174b]
    Former-commit-id: 3cfe0ae56ce3f0c080aaaa09519f99b848569a7c

---
+++ bizbox-front/src/components/home/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <div class="col-s-12 col-2"><Select :idx="items1.idx" :items="items1.item"></Select></div>
+    <div class="col-s-12 col-2"><Select :idx="items2.idx" :items="items2.item"></Select></div>
+  </div>
+</template>
+
+<script>
+import Select from '@/components/common/Select.vue'
+export default {
+  data () {
+    return {
+      items1: {
+        idx: '시/도',
+        item: ['서울', '부산', '울산']
+      },
+      items2: {
+        idx: '구/군',
+        item: ['강남구', '강서구', '강북구']
+      }
+    }
+  },
+  components: {
+    Select
+  }
+}
+</script>
+
+<style scoped src="./SearchBar.css"></style>
