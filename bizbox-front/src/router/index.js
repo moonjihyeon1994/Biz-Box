1cfc501383995b643420dbe6c33e4c0e2fdc6f0e
문요한 <munyohan@naver.com>
2020-01-28 17:55:01 +0900

    200128_문요한_매출 차트 컴포넌트 만듦
    Former-commit-id: 2766404d68ae25da43fe329bae493c583d7211bf [formerly 893ba1ea7214190a81a7e6674b6796b26606be77]
    Former-commit-id: cf0155445444ea28e36173170cbb7b9f68052def

--- bizbox-front/src/router/index.js
+++ bizbox-front/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/components/home/Home.vue'
 import Result from '@/components/result/Result.vue'
-import Chart from '@/components/result/Chart.vue'
 
 Vue.use(VueRouter)
 
@@ -25,11 +24,6 @@ const routes = [
     path: '/result',
     name: 'result',
     component: Result
-  },
-  {
-    path: '/chartjs',
-    name: 'chartjs',
-    component: Chart
   }
 ]
 
