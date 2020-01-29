ccb6d51da8251a6407515082023c469dfd7f5f62
osw <tmddhks4717@naver.com>
2020-01-29 17:14:13 +0900

    -
    Former-commit-id: 1b4858cadbc3049d3695774538e235b0cd54d171 [formerly 4f15ffa7fd1158ced9ddcc1f031d238b5e9ff392]
    Former-commit-id: bd75d11748cc94b6b417ba74a0d6d74ad3df91cc

---
+++ BizBox_back/src/main/java/com/bizbox/controller/IndustrySuggestionsController.java
@@ -0,0 +1,34 @@
+package com.bizbox.controller;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bizbox.Service.ChangeBusinessService;
+import com.bizbox.Service.PopulationByService;
+import com.bizbox.vo.Changebusiness;
+import com.bizbox.vo.PopulationByLocation;
+import com.bizbox.vo.PopulationByTime;
+import com.bizbox.utils.*;
+import net.bytebuddy.dynamic.scaffold.MethodGraph.Linked;
+import com.bizbox.apis.*;
+
+
+@CrossOrigin({ "*" })
+@RestController
+@RequestMapping("/change")
+public class IndustrySuggestionsController {
+	
+	
+}
