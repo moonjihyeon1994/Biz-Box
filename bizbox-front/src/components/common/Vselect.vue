eebcf8d069457f99779faa8e0bfed473d9dc2bc9
JaehyunKim94 <didwjd169@gmail.com>
2020-01-29 18:05:09 +0900

    200129_김재현_front styling save
    Former-commit-id: c889d96f955535c404d1e275744f6a8c812de620 [formerly e6d18505c5d1b5ab0b3546570dfc1d4b18a389f1]
    Former-commit-id: 342ad4ac9d72def2f5a36a9aaa0f2b605808672b

---
+++ bizbox-front/src/components/common/Vselect.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-select :items="items" :label="labname"></v-select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array,
+    labname: String
+  }
+}
+</script>
+
+<style scoped src="./Vselect.css"></style>
---
+++ bizbox-front/src/components/home/Vsearch.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Vselect :items="items1" :labname="labname1" class="flex-column-1"></Vselect>
+    <Vselect :items="items2" :labname="labname2" class="flex-column-1"></Vselect>
+  </div>
+</template>
+
+<script>
+import Vselect from '@/components/common/Vselect.vue'
+export default {
+  data: () => ({
+    labname1: '시/도',
+    items1: ['서울', '부산', '울산'],
+    labname2: '구/군',
+    items2: ['강남구', '강서구', '강북구']
+  }),
+  components: {
+    Vselect
+  }
+}
+</script>
+
+<style scoped src="./Vsearch.css"></style>
