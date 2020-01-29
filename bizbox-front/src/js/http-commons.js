ec57b8afb284f2ce10012cb4a64ac9716d314580
Eumyounghyun <opwer032@naver.com>
2020-01-29 17:48:22 +0900

    200129  불필요 데이터 삭제
    Former-commit-id: f1c68aabaca084889af5c94ca0b2f3a3fcdeb9f8 [formerly ad15809c9f973ae8047fe27b027c7bc6dd920c75]
    Former-commit-id: 03e6b04a4d6c0cd9964ead4e09e254edfa208b81

--- bizbox-front/src/js/http-commons.js
+++ bizbox-front/src/js/http-commons.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
   // baseURL: 'http://70.12.246.137:8080',
-  baseURL: 'http://70.12.247.78:8080',
+  baseURL: 'http://13.125.20.125:8081',
   headers: {
     'Content-type': 'application/json'
   }
