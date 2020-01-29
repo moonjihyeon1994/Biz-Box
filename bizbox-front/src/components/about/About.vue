d7526188f0fea006a68dfceb6a623a4c00cd565b
heecheol <heecheol1508@gmail.com>
2020-01-29 16:26:08 +0900

    200129 양희철 카카오로그인  - sessionStorage 적용  - jwt token 관리
    Former-commit-id: 4e2eca157494c3f41cd9db766d5f66cfedafb5e2 [formerly 87b2c706ea1e86f38822db72d1931de2909c78bd]
    Former-commit-id: d2db5e85e2d4941432486e08daa1896ac642f6d3

--- bizbox-front/src/components/about/About.vue
+++ bizbox-front/src/components/about/About.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <!-- <v-row>
-      <resultList></resultList>
-    </v-row> -->
     <v-row>
       <Login></Login>
+    </v-row>
+    <v-row>
+      <resultList></resultList>
     </v-row>
   </div>
 </template>
 <script>
-// import resultList from '@/components/about/resultList.vue'
+import resultList from '@/components/about/resultList.vue'
 import Login from '@/components/common/Login.vue'
 export default {
   components: {
-    // resultList,
+    resultList,
     Login
   }
 }
