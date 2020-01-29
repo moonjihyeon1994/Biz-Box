d227518672d3555ef585783535f184354bc35dc2
osw <tmddhks4717@naver.com>
2020-01-29 11:39:16 +0900

    200129 매출정보 동검색 가능하도록 수정 및 도로명주소에서 숫자 제거하는 utill 패키지 추가 osw
    Former-commit-id: 5abd4b38395584f2bdafccae01ab2f1cebc4d433 [formerly ba6f35ad729a0aad14e820f01273ad6fc10403f2]
    Former-commit-id: 1f6f7019dc6d6990f305a419e94fe1af3e05ec7c

--- BizBox_back/src/main/java/com/bizbox/utils/AddressUtil.java
+++ BizBox_back/src/main/java/com/bizbox/utils/AddressUtil.java
@@ -5,8 +5,14 @@ public class AddressUtil {
 	
 	public String RemoveNumber(String address) {
 		
+		String removedAddress =address.replaceAll("[0-9]", "");
+		System.out.println(removedAddress.substring(removedAddress.length()-1,removedAddress.length()));
+		if(removedAddress.substring(removedAddress.length()-1,removedAddress.length()).contentEquals("길")) {
+			removedAddress=removedAddress.substring(0, removedAddress.length()-1);
+		}
+		System.out.println(address+" "+ "==>" +" "+removedAddress);
 		
-		return address;
+		return removedAddress;
 	}
 	
 }
