c160a45379002256a0a2170b7cd88a2e192c310f
JaehyunKim94 <didwjd169@gmail.com>
2020-01-30 15:15:32 +0900

    200130_김재현_login router
    Former-commit-id: a16ba276fa95f8609148672de33b9f2b783d8e76 [formerly c72bb66f16c57530572e6832e985808959e39164]
    Former-commit-id: 5e0dff7689a37660c89883e820a1ab0f63eb5e68

--- bizbox-front/src/components/newsCrawling/news.vue
+++ bizbox-front/src/components/newsCrawling/news.vue
@@ -1,53 +1,53 @@
 <template>
-<div>
+  <div>
     <div class="newsrow" v-for="news in newses" :key="news.title">
-        <span>
-              "<a :href="news.url" target="_blank">
-                {{news.title}}
-              </a>"
-        </span>
+      <span>
+        "<a :href="news.url" target="_blank">
+          {{ news.title }} </a
+        >"
+      </span>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 import axios from '../../js/http-commons'
 
 export default {
-    data(){
-        return{
-            newses: []
-        }
-    },
-    mounted(){
-        this.getNews()
-    },
-    methods: {  
-        getNews(){  
-            axios
-            .get('/crawling/naver')
-            .then(res=>{
-                this.newses = res.data.headlines;
-            })
-            .catch(err=>{
-                alert(err, '불러올 뉴스 데이터가 없습니다.')
-            })
-        }       
+  data () {
+    return {
+      newses: []
     }
+  },
+  mounted () {
+    this.getNews()
+  },
+  methods: {
+    getNews () {
+      axios
+        .get('/crawling/naver')
+        .then(res => {
+          this.newses = res.data.headlines
+        })
+        .catch(err => {
+          alert(err, '불러올 뉴스 데이터가 없습니다.')
+        })
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
 .newsrow {
-        margin: 10px;
-       }
+  margin: 10px;
+}
 .newsrow a {
-        text-decoration: none;
-        color: black;
-        size: 2em;
-        text-shadow: 0 0 12px;
-      }
+  text-decoration: none;
+  color: black;
+  size: 2em;
+  text-shadow: 0 0 12px;
+}
 .newsrow span {
-        size: 10px;
-      }
+  size: 10px;
+}
 </style>
