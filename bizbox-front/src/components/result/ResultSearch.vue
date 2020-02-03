476bfb09ac3ad384611725be693d72866bf6c30f
문요한 <munyohan@naver.com>
2020-02-03 09:14:48 +0900

    200203_문요한_Git 옮기기 전 최종 커밋
    Former-commit-id: 36f35faaaaaefb6c710fdd7f53ab720802fea884 [formerly 5fb6a6b15afec92fa836dab0ef8f5cea95cf87e1]
    Former-commit-id: 78c65c230ff4f67514f5ee9377073064c4780d09

--- bizbox-front/src/components/result/ResultSearch.vue
+++ bizbox-front/src/components/result/ResultSearch.vue
@@ -1,7 +1,32 @@
 <template>
-  <div id="obj">
-    <div id="box">
+  <div id="container">
+    <div id="selBoxes">
+      <select id="sel1">
+        <option value="title">시/도</option>
+        <option value="seoul">서울</option>
+        <option value="incheon">인천</option>
+        <option value="gyeonggi">경기</option>
+      </select>
 
+      <select id="sel2">
+        <option value="title">구/군</option>
+        <option value="gangnam">강남구</option>
+        <option value="mapo">마포구</option>
+      </select>
+
+      <select id="sel3">
+        <option value="title">업종</option>
+        <option value="cafe">카페</option>
+        <option value="karaoke">노래방</option>
+        <option value="chicken">치킨</option>
+      </select>
+    </div>
+
+    <input id="searchBox" placeholder="도로명 주소를 입력해주세요.">
+
+    <div id="btns">
+      <button id="resetBtn">초기화</button>
+      <button id="searchBtn">검색</button>
     </div>
   </div>
 </template>
@@ -17,4 +42,4 @@ export default {
 }
 </script>
 
-<style scoped src="@/style/ResultSearch.scss" lang="scss"></style>
+<style scoped src="./ResultSearch.scss" lang="scss"></style>
