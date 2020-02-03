0ea87f688cbf7319edb309db0a52c5ec3ff5ab2b
Eumyounghyun <opwer032@naver.com>
2020-02-03 14:45:07 +0900

    200203 map 기본화면 완성
    Former-commit-id: 66e1a60d163edc37e7108836b392f8239856f5f5 [formerly 23abeaf989eae16eec188cde989c035fa6253e33]
    Former-commit-id: 93167746e967a0ce21e2fc2da0f0da50bb099131

--- bizbox-front/src/components/map/kakaomap.vue
+++ bizbox-front/src/components/map/kakaomap.vue
@@ -1,42 +1,58 @@
-<template>    
-<!-- 지도를 표시할 div 입니다 -->
-<div>
-    <span @click="this.onLoad">지도</span>
-<vue-daum-map
+<template>
+  <!-- 지도를 표시할 div 입니다 -->
+  <div class="mapContainer">
+    <vue-daum-map
+      class="map"
       :appKey="this.appKey"
       :center.sync="this.center"
       :level.sync="this.level"
       :mapTypeId="this.mapTypeId"
       :libraries="this.libraries"
       @load="this.onLoad"
-      style="width:500px;height:400px;"/>
-</div>
-
-
+    />
+  </div>
 </template>
 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=0574c7ce26ff4134a0dc5f831d6edd37&libraries=services"></script>
 <script>
-import VueDaumMap from 'vue-daum-map'   
+import VueDaumMap from 'vue-daum-map'
 
 export default {
-    data: () => ({
-        appKey: '8227ff53a7c3554b706e45589733cb41', // 테스트용 appkey
-        center: {lat:33.450701, lng:126.570667}, // 지도의 중심 좌표
-        level: 3, // 지도의 레벨(확대, 축소 정도),
-        mapTypeId: VueDaumMap.MapTypeId.NORMAL, // 맵 타입
-        libraries: [], // 추가로 불러올 라이브러리
-        map: null // 지도 객체. 지도가 로드되면 할당됨.
-    }),
-    methods: {
-        // 지도가 로드 완료되면 load 이벤트 발생
-        onLoad (map) {
-            alert("로드됨");
-            this.map = map
-        }
+  data: () => ({
+    appKey: '8227ff53a7c3554b706e45589733cb41', // 테스트용 appkey
+    center: { lat: 37.5013068, lng: 127.037471 }, // 지도의 중심 좌표
+    level: 3, // 지도의 레벨(확대, 축소 정도),
+    mapTypeId: VueDaumMap.MapTypeId.NORMAL, // 맵 타입
+    libraries: [], // 추가로 불러올 라이브러리
+    map: null // 지도 객체. 지도가 로드되면 할당됨.
+  }),
+  methods: {
+    // 지도가 로드 완료되면 load 이벤트 발생
+    onLoad(map) {
+      this.map = map
     }
+  },
+  components: {
+    VueDaumMap
+  }
 }
 </script>
 
 <style>
-
+.mapContainer {
+  position: fixed;
+  top: -50%;
+  left: -50%;
+  width: 200%;
+  height: 200%;
+}
+.map {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    min-width: 50%;
+    min-height: 50%;
+}
 </style>
