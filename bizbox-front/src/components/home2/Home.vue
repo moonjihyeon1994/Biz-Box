6b4f896f9463d92e350e6bc9af6cc93f69d89ca7
JaehyunKim94 <didwjd169@gmail.com>
2020-02-03 13:47:43 +0900

    200203_김재현_homepage init
    Former-commit-id: bdd669c0533b77e634ccda1ad0baba275bddd916 [formerly 64011912bb35eaf151f5dc23f70b1e4f2b92c064]
    Former-commit-id: 57393453fa2cdb9f12de6d25844dfd7566dd0584

---
+++ bizbox-front/src/components/home2/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="flex-item-1">
+      <h1>hello</h1>
+    </div>
+    <div class="flex-item-1">
+      <h1>hello</h1>
+    </div>
+    <div class="flex-item-1">
+      <h1>hello</h1>
+    </div>
+    <div class="flex-item-1">
+      <h1>hello</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped src="./Home.css"></style>
