579e4e4a955c5f4e0fc3f61b993e72c843200c1b
문요한 <munyohan@naver.com>
2020-02-04 18:32:50 +0900

    200204_문요한_About컴포넌트 작업
    Former-commit-id: 8f0e3e5344b3d9c8b81e364e773e9026a597ef41 [formerly 1ea8ab9f8efdea3ab122371757f73478990259d6]
    Former-commit-id: c6bb7abe4000371bdd749ab573199daa4b77e7be

--- bizbox-front/src/components/home/about/About.vue
+++ bizbox-front/src/components/home/about/About.vue
@@ -1,7 +1,44 @@
 <template>
   <div id="bz-container">
-    <div id="bz-styleBox"></div>
-    <div>쉬운 상권 분석</div>
+    <div id="bz-about1">
+      <div class="bz-imgBox">
+        <img src="@/assets/home_img1.png">
+      </div>
+      <div class="colorBox"></div>
+      <div class="bz-aboutTitle">
+        믿을 수 있는 데이터
+      </div>
+      <div class="bz-aboutText">
+        대한민국 정부에서 제공하는 공공 데이터를 기반으로 만들었기에 믿을 수 있습니다.
+      </div>
+    </div>
+
+    <div id="bz-about2">
+      <div class="bz-imgBox">
+        <img src="@/assets/home_img2.png">
+      </div>
+      <div class="colorBox"></div>
+      <div class="bz-aboutTitle">
+      <span style="color:#ddd">방대</span>한 양의 상권 정보
+      </div>
+      <div class="bz-aboutText">
+        약 200만건에 해당하는 탄탄한 데이터를 바탕으로 만들어졌습니다. 더욱 자세하고 정확한 정보를 얻을 수 있습니다.
+      </div>
+    </div>
+
+    <div id="bz-about3">
+      <div class="bz-imgBox">
+        <img src="@/assets/home_img3.png">
+      </div>
+      <div class="colorBox"></div>
+      <div class="bz-aboutTitle">
+        분석하기 쉬운 디자인
+      </div>
+      <div class="bz-aboutText">
+        지도와 차트를 이용하여 누구라도 쉽게 분석할 수 있도록 구성했습니다.
+      </div>
+    </div>
+
   </div>
 </template>
 
