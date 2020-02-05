df8d45d50ebf18237ce914a6b12e15961dcde246
osw <tmddhks4717@naver.com>
2020-02-05 18:34:02 +0900

    충돌문제 해결해야함 오승완
    Former-commit-id: ab4e3a042493269ae6c5280405a17eff582f5741 [formerly 8fdd5367a179fc6cdd3caf9aabf3ef9fbfdb32eb]
    Former-commit-id: 206c4251b583efe704bdd9ab562a7bf64323dd7d

--- bizbox-front/src/router/index.js
+++ bizbox-front/src/router/index.js
@@ -3,18 +3,13 @@ import VueRouter from 'vue-router'
 import Signup from '@/components/signup/Signup.vue'
 import Login from '@/components/login/Login.vue'
 import Home from '@/components/home/Home.vue'
-<<<<<<< HEAD
-import About from '@/components/about/About.vue'
-import Result from '@/components/result/Result.vue'
-import Login from '@/components/common/Login.vue'
-import Signup from '@/components/common/Signup.vue'
-import ResultSearch from '@/components/result/ResultSearch.vue'
+// import About from '@/components/about/About.vue'
+// import Result from '@/components/result/Result.vue'
+// import ResultSearch from '@/components/result/ResultSearch.vue'
 import map from '@/components/map/Newmap.vue'
-=======
 import BizMap from '@/components/bizmap/BizMap'
 import Modal from '@/components/modal/Modal.vue'
 
->>>>>>> 3cf1ed59ee00b2a0ef67a06e86d5054f92bc03d8
 Vue.use(VueRouter)
 
 const routes = [
@@ -33,17 +28,17 @@ const routes = [
     name: 'signup',
     component: Signup
   },
-  {
-<<<<<<< HEAD
-    path: '/result2',
-    name: 'newResult',
-    component: ResultSearch
-  },
+  // {
+  //   path: '/result2',
+  //   name: 'newResult',
+  //   component: ResultSearch
+  // },
   {
     path: '/map',
     name: 'newResult',
     component: map
-=======
+  },
+  {
     path: '/bizmap',
     name: 'bizmap',
     component: BizMap
@@ -52,7 +47,6 @@ const routes = [
     path: '/modal',
     name: 'modal',
     component: Modal
->>>>>>> 3cf1ed59ee00b2a0ef67a06e86d5054f92bc03d8
   }
   // 바꿈
 ]
