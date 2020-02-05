ddffa5416909da82c59180f9bf5a8ad7169723a9
문지현 <puppy613@hanmail.net>
2020-02-05 11:38:41 +0900

    0205 캐시 추가
    Former-commit-id: 64696917128615bbf687f550461cc429346de7b0 [formerly f9b34c2ff01d9e8c71bff9c105c05ad74a532494]
    Former-commit-id: e808e7502b7090a96590531746a62dc24872c363

--- BizBox_back/src/main/java/com/bizbox/controller/GlobalRestExceptionHandler.java
+++ BizBox_back/src/main/java/com/bizbox/controller/GlobalRestExceptionHandler.java
@@ -6,20 +6,17 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestControllerAdvice
-@Slf4j
 public class GlobalRestExceptionHandler {
-	@ExceptionHandler(value = {RuntimeException.class})
+
+	@ExceptionHandler(value = { RuntimeException.class })
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public Map<String, Object> internalServerError(Exception e){
+	public Map<String, Object> internalServerError(Exception e) {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("msg", e.getMessage());
 		return resultMap;
 	}
-	
+
 }
