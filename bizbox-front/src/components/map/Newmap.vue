55a9603f1fe7001b46974d1eb09717f0fdbc7435
osw <tmddhks4717@naver.com>
2020-02-06 08:31:01 +0900

    200206 각 동별 폴리곤 클릭시 동이름 및 위치이동 오승완
    Former-commit-id: 15e0184ad3f88962e3e0ca8058c46f147fb55c5e [formerly 44243d5115033d17d624e7b8ea2ac037dd115816]
    Former-commit-id: 5df13e329a3b885f559a6abefa6507941596793d

--- bizbox-front/src/components/map/Newmap.vue
+++ bizbox-front/src/components/map/Newmap.vue
@@ -2,8 +2,8 @@
   <div class="mapContainer">
    <div  class="a ssearchbox" >
     <input v-model="name" type="text" placeholder="검색어입력">
-    <button  v-on:click="asdf">검색</button>
-    <textarea id="result" v-model="message"></textarea>
+    <button  v-on:click="serch">검색</button>
+    <textarea style="width:300px;" id="result" v-model="message"></textarea>
   </div>
     <div class="map" id="map" style="width:500px;height:400px;"></div>
   </div>
@@ -29,9 +29,9 @@ export default {
   mounted () {
     let data = Dong.features //좌표 저장할 배열
     let coordinates = []      //행정 구 이름
-    let name = ''
+    let name = '멀티캠퍼스'
     let Message = ''
-    let pp = null;
+    let pp = null
     this.message = Message
     var container = document.getElementById('map')
 
@@ -39,91 +39,98 @@ export default {
       center: new kakao.maps.LatLng(37.505691, 127.0298106),
       level: 3
     }
+    // --코더 생성------------------------------------------------------------------------------------
     this.geocoder = new kakao.maps.services.Geocoder()// 코더생성
+    // --맵 생성--------------------------------------------------------------------------------------
     this.map = new kakao.maps.Map(container, options)// 맵 생성
+    kakao.maps.event.addListener(this.map, 'bounds_changed', () => {
+      var bounds = Map.getBounds() // 지도 영역정보
+      var swLatlng = bounds.getSouthWest()// 영역정보의 남서쪽 정보
+      var neLatlng = bounds.getNorthEast()// 영역정보의 북동쪽 정보
+      this.message = '영역좌표 :' + swLatlng.toString() + '/' + neLatlng.toString() + ''
+    })
     let Map = this.map
-    let vm=this
-    for (var i = 0, len = data.length; i < len; i++) {
-      this.coordinates = data[i].geometry.coordinates
-      name = data[i].properties.ADM_DR_NM
-      // console.log(data)
-      // console.log(data[0].geometry.coordinates)
-      displayArea(vm.pp, this.map, this.coordinates[0][0], name, this.coordinates[0][0].length)
-    }
-
-    // this.polygon = []
-
-    // 다각형을 생상하고 이벤트를 등록하는 함수입니다
-    function displayArea (polygon, Mmap, coordinates, name, length) {
-      let path = []
-      let points = []
-      // console.log(coordinates)
-      for (let i = 0; i < length; i++) {
-        console.log(coordinates.length)
-    
-        console.log(coordinates[0])
-        console.log(coordinates[i][1])
-        console.log(coordinates[i][0])
-        
-        path.push(new kakao.maps.LatLng(coordinates[i][1], coordinates[i][0]))
-      }
-      console.log(path.length)
-      console.log(Mmap)
-      console.log(path)
-      console.log(name)
-      
-      polygon = new kakao.maps.Polygon({
-        map: Mmap, // 다각형을 표시할 지도 객체
-        path: path,
-        strokeWeight: 2,
-        strokeColor: '#004c80',
-        strokeOpacity: 0.8,
-        fillColor: '#fff',
-        fillOpacity: 0.7
-      })
-      console.log(pp)
-    }
-    console.log(this.polygon)
+    let vm = this
+    // --마커 생성--------------------------------------------------------------------
     this.marker = new kakao.maps.Marker({// 마커생성
       map: this.map,
-      position: new kakao.maps.LatLng(37.505691, 127.0298106)
+      position: new kakao.maps.LatLng(37.505691, 127.0298106) // 최초 표시되는 마커의 위치
     })
+
+    // --인포윈도우 생성---------------------------------------------------------------
     this.infowindow = new kakao.maps.InfoWindow({
-      content:
-             this.name })
+      content: 'name' })
+    // -------------------------------------------------------------------------------
+
+    // --폴리곤 생성---------------------------------------------------------------------------------------
+    for (var i = 0, len = data.length; i < len; i++) { // 동의 경계면 좌표를 받아서 다각형 생성 함수에 전달
+      this.coordinates = data[i].geometry.coordinates
+      name = data[i].properties.ADM_DR_NM
+      displayArea(vm.pp, this.map, this.coordinates[0][0], name, this.coordinates[0][0].length)
+    }
+    function displayArea (polygon, Mmap, coordinates, name, length) { // 다각형을 생성 ,이벤트를 등록
+      let path = []
+      for (let i = 0; i < length; i++) { // 좌표의 개수만큼 반복문을 돌며 경계 바운더리 생성(path생성)
+        path.push(new kakao.maps.LatLng(coordinates[i][1], coordinates[i][0]))
+      }
+      polygon = new kakao.maps.Polygon({ // 생성된 path를 이용하여 폴리곤(다각형) 생성
+        map: Mmap, // 다각형을 표시할 지도 객체
+        path: path,
+        strokeWeight: 3, // 선두깨
+        strokeColor: '#004c80', // 선색
+        strokeOpacity: 0.6, // 선 투명도
+        fillColor: '#fff',
+        fillOpacity: 0.4
+      })
+
+      kakao.maps.event.addListener(polygon, 'mouseover', () => { // 각 폴리곤에 마우스 오버 이벤트 등록
+        polygon.setOptions({
+          fillColor: '#09f'
+        })
+      })
+      kakao.maps.event.addListener(polygon, 'mouseout', () => { //  각 폴리곤에 마우스 아웃 이벤트 등록
+        polygon.setOptions({
+          fillColor: '#fff'
+        })
+      })
+      kakao.maps.event.addListener(polygon, 'click', () => { //  각 폴리곤에 마우스 아웃 이벤트 등록
+        let Name = name
+        let Marker = vm.marker
+        let InfoWindow = vm.infowindow
+        vm.geocoder.addressSearch(Name, function (
+          result,
+          status
+        ) {
+        // 정상적으로 검색이 완료되면
+          if (status === kakao.maps.services.Status.OK) {
+            console.log(Name)
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x)// 결과값으로 받은 위치를 마커의 위치로 적용
+            Marker.setPosition(coords)
+            InfoWindow.close()
+            InfoWindow.setContent(Name)
+            InfoWindow.open(Map, Marker)
+            Map.setCenter(coords)
+          }
+        })
+      })
+    }
     this.ifchanege = this.map.getCenter()
-    // console.log(kakao)
-    kakao.maps.event.addListener(this.map, 'bounds_changed', () => {
-      // 지도 영역정보를 얻어옵니다
-      var bounds = Map.getBounds()
-      // 영역정보의 남서쪽 정보를 얻어옵니다
-      var swLatlng = bounds.getSouthWest()
-      // 영역정보의 북동쪽 정보를 얻어옵니다
-      var neLatlng = bounds.getNorthEast()
-      this.message = '영역좌표는 남서쪽 위도, 경도는  ' + swLatlng.toString()  + '북동쪽 위도, 경도는  ' + neLatlng.toString() + '입니다 <'
-      console.log(this.message)
-      // this.message = '영역좌표는 남서쪽 위도, 경도는  ' + swLatlng.toString()  + '북동쪽 위도, 경도는  ' + neLatlng.toString() + '입니다 <'
-    })
     var resultDiv = document.getElementById('result')
     resultDiv.innerHTML = Message
   },
   watch: {
     ifchanege: function (newVal, oldVal) {
-      alert('!!!!!!!!!!!')
     }
   },
   components:{
   },
   methods: {
-    panTo () {
-      // 이동할 위도 경도 위치를 생성합니다
-      var moveLatLon = new kakao.maps.LatLng(33.45058, 126.574942)
-      // 지도 중심을 부드럽게 이동시킵니다
-      // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    panTo () { // 지도 중심을 부드럽게 이동시킵니다
+      var moveLatLon = new kakao.maps.LatLng(33.45058, 126.574942) // 이동할 위도 경도 위치를 생성합니다
       this.map.panTo(moveLatLon)
     },
-    asdf (name) {
-      // eslint-disable-next-line no-unused-vars
+    // -- 동이름으로 검색-----------------------------------------------------------------------------
+    serch (name) {
       let Ifchange = this.ifchanege
       let Name = this.name
       let Map = this.map
@@ -133,25 +140,18 @@ export default {
         result,
         status
       ) {
-        // 정상적으로 검색이 완료됐으면
+        // 정상적으로 검색이 완료되면
         if (status === kakao.maps.services.Status.OK) {
           var coords = new kakao.maps.LatLng(result[0].y, result[0].x)// 결과값으로 받은 위치를 마커의 위치로 적용
           Marker.setPosition(coords)
-
-          // 인포윈도우로 장소에 대한 설명을 표시합니다
-          // var infowindow = new kakao.maps.InfoWindow({
-          //   content:
-          //    Name
-          // })
-          InfoWindow.close()
-          InfoWindow.setContent(Name)
-          InfoWindow.open(Map, Marker)
-          Ifchange = Map.getCenter()
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-          Map.setCenter(coords)
+          InfoWindow.close() // 전에 있던 인포위도우 클로즈
+          InfoWindow.setContent(Name) //  인포위도우 내용 세팅
+          InfoWindow.open(Map, Marker) // 마커위에 인포위도우 열림
+          Map.setCenter(coords) // 새로 세팅된 센터 값으로 맵 세팅
         }
       })
     }
+    // -----------------------------------------------------------------------------------------
   }
 }
 </script>
