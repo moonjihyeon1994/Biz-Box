6286f1cd23a534fe713332ccf38cfdc5a4f27d93
osw <tmddhks4717@naver.com>
2020-02-07 09:56:16 +0900

    map 합침 오승완
    Former-commit-id: 646336d10451f801ae213571df851194ce360659 [formerly 6967ceab7d01dfaa15dbf69a5e366e67d861e60e]
    Former-commit-id: 8950f477c75a32cfd1f128f7117ed5985a28a8bc

--- bizbox-front/src/components/map/Newmap.vue
+++ bizbox-front/src/components/map/Newmap.vue
@@ -19,19 +19,13 @@
     <condition></condition>
     <div class="map" id="map"></div>
   </div>
-  <!-- <div class="mapContainer">
-   <div  class="a ssearchbox" >
-    <input v-model="name" type="text" placeholder="검색어입력">
-    <button  v-on:click="serch">검색</button>
-    <textarea style="width:300px;" id="result" v-model="message"></textarea>
-  </div>
-    <div class="map" id="map" style="width:500px;height:400px;"></div>
-  </div>-->
 </template>
 
 <script>
 import Dong from '../../assets/json/New.json'
 import condition from '@/components/bizmap/kakaomap/SearchCondition.vue'
+import axios from '../../js/http-commons'
+
 export default {
   data: () => {
     return {
@@ -51,10 +45,24 @@ export default {
       drawingCircle: false, // 그려지고 있는 원을 표시할 원 객체
       drawingLine: false, // 그려지고 있는 원의 반지름을 표시할 선 객체
       drawingOverlay: false, // 그려지고 있는 원의 반경을 표시할 커스텀오버레이
-      circles: []
+      circles: [],
+      countResult: '',
+      searchX: '',
+      searchY: '',
+      range: '',
+      CountInfo: {
+        소매: '',
+        학문교육: '',
+        숙박: '',
+        생활서비스: '',
+        음식: '',
+        부동산: '',
+        의료: '',
+        관광여가오락: ''
+      }
     }
   },
-  mounted () {
+  mounted() {
     let data = Dong.features // 좌표 저장할 배열
     let coordinates = [] // 행정 구 이름
     let name = '멀티캠퍼스'
@@ -80,7 +88,6 @@ export default {
     })
     let Map = this.map
     let vm = this
-    //let Mode = this.$store.state.mode
     // --마커 생성--------------------------------------------------------------------
     this.marker = new kakao.maps.Marker({
       // 마커생성
@@ -106,7 +113,6 @@ export default {
     })
     // -------------------------------------------------------------------------------------
 
-
     // --폴리곤 생성---------------------------------------------------------------------------------------
     for (var i = 0, len = data.length; i < len; i++) {
       // 동의 경계면 좌표를 받아서 다각형 생성 함수에 전달
@@ -125,26 +131,34 @@ export default {
       // 다각형을 생성 ,이벤트를 등록
       let mode = vm.$store.state.mode
       let path = []
+      let points = []
       for (let i = 0; i < length; i++) {
         // 좌표의 개수만큼 반복문을 돌며 경계 바운더리 생성(path생성)
+        var point = new Object()
+        point.x = coordinates[i][1]
+        point.y = coordinates[i][0]
+        points.push(point)
         path.push(new kakao.maps.LatLng(coordinates[i][1], coordinates[i][0]))
       }
       polygon = new kakao.maps.Polygon({
         // 생성된 path를 이용하여 폴리곤(다각형) 생성
         map: Mmap, // 다각형을 표시할 지도 객체
         path: path,
-        strokeWeight: 3, // 선두깨
+        strokeWeight: 2, // 선두깨
         strokeColor: '#004c80', // 선색
-        strokeOpacity: 0.6, // 선 투명도
+        strokeOpacity: 0.4, // 선 투명도
         fillColor: '#fff',
-        fillOpacity: 0.4
+        fillOpacity: 0.2
       })
 
-      kakao.maps.event.addListener(polygon, 'mouseover', () => {
+      kakao.maps.event.addListener(polygon, 'mouseover', mouseEvent => {
         // 각 폴리곤에 마우스 오버 이벤트 등록
+        let Name = name
+        let position = mouseEvent.LatLng
         polygon.setOptions({
           fillColor: '#09f'
         })
+        vm.overlay(Name, position)
       })
       kakao.maps.event.addListener(polygon, 'mouseout', () => {
         //  각 폴리곤에 마우스 아웃 이벤트 등록
@@ -174,8 +188,6 @@ export default {
       })
     }
     this.ifchanege = this.map.getCenter()
-    // var resultDiv = document.getElementById('result')
-    // resultDiv.innerHTML = Message
   },
   watch: {
     ifchanege: function(newVal, oldVal) {}
@@ -184,13 +196,20 @@ export default {
     condition
   },
   methods: {
-    panTo() {
+    overlay (name, position) {
+      var customOverlay = new kakao.maps.CustomOverlay({
+        position: position,
+        content: '<div class="area">' + name + '</div>'
+      })
+      customOverlay.setMap(this.map)
+    },
+    panTo () {
       // 지도 중심을 부드럽게 이동시킵니다
       var moveLatLon = new kakao.maps.LatLng(33.45058, 126.574942) // 이동할 위도 경도 위치를 생성합니다
       this.map.panTo(moveLatLon)
     },
     // -- 동이름으로 검색-----------------------------------------------------------------------------
-    serch(name) {
+    serch (name) {
       let Ifchange = this.ifchanege
       let Name = this.name
       let Map = this.map
@@ -208,7 +227,7 @@ export default {
         }
       })
     },
-    CircleMouseClick(mouseEvent) {
+    CircleMouseClick (mouseEvent) {
       // 지도에 클릭 이벤트를 등록
 
       this.removeCircles()
@@ -217,6 +236,10 @@ export default {
           // 클릭 이벤트가 발생했을 때 원을 그리고 있는 상태가 아니면 중심좌표를 클릭한 지점으로 설정
           this.drawingFlag = true
           this.centerPosition = mouseEvent.latLng // 원이 그려질 중심좌표를 클릭한 위치로 설정합니다
+
+          this.searchX = this.centerPosition.Ga
+          this.searchY = this.centerPosition.Ha
+
           if (!this.drawingLine) {
             // 그려지고 있는 원의 반경을 표시할 선 객체를 생성합니다
             this.drawingLine = new kakao.maps.Polyline({
@@ -302,7 +325,8 @@ export default {
           fillOpacity: 0.2 // 채우기 불투명도입니다
         })
         var radius = Math.round(circle.getRadius()) // 원의 반경 정보를 얻어옵니다
-        var html = this.getBoxHTML(radius) // 커스텀 오버레이에 표시할 반경 정보입니다
+        this.range = radius
+        var html = this.getBoxHTML() // 커스텀 오버레이에 표시할 반경 정보입니다
         var radiusOverlay = new kakao.maps.CustomOverlay({
           // 반경정보를 표시할 커스텀 오버레이를 생성합니다
           content: html, // 표시할 내용입니다
@@ -330,21 +354,123 @@ export default {
       }
       this.$store.state.mode = 0
     },
-    getBoxHTML (distance) {
+    getBoxHTML () {
+      this.getData()
+      console.log(this.CountInfo)
+      let 소매 = this.CountInfo.소매
+      if (소매 === undefined) {
+        소매 = 0
+      }
+      let 학문교육 = this.CountInfo.학문교육
+      if (학문교육 === undefined) {
+        학문교육 = 0
+      }
+      let 숙박 = this.CountInfo.숙박
+      if (숙박 === undefined) {
+        숙박 = 0
+      }
+      let 생활서비스 = this.CountInfo.생활서비스
+      if (생활서비스 === undefined) {
+        생활서비스 = 0
+      }
+      let 음식 = this.CountInfo.음식
+      if (음식 === undefined) {
+        음식 = 0
+      }
+      let 부동산 = this.CountInfo.부동산
+      if (부동산 === undefined) {
+        부동산 = 0
+      }
+      let 의료 = this.CountInfo.의료
+      if (의료 === undefined) {
+        의료 = 0
+      }
+      let 관광여가오락 = this.CountInfo.관광여가오락
+      if (관광여가오락 === undefined) {
+        관광여가오락 = 0
+      }
       var content =
-        '<div class="v-sheet theme--light elevation-14" style="margin-leftauto;display:block;text-align:center;" id="mapSheet">'
+        '<div class="v-sheet theme--light elevation-14" style="width:300px;height:150px;margin:auto;display:block;text-align:center;" id="mapSheet">'
+      content +=
+        '    <div style="padding-top:10px;display:flex;justify-content:space-around;">'
+      content += '    <div style="width:70px;height:70px;">'
+      content +=
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '<span style="width:100%;">' +
+        소매
+      content += '</span></div>'
+      content += '    <div style="width:70px;height:70px;">'
+      content +=
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '<span style="width:100%;">' +
+        학문교육
+      content += '</span></div>'
+      content += '    <div style="width:70px;height:70px;">'
+      content +=
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '<span style="width:100%;">' +
+        숙박
+      content += '</span></div>'
+      content += '    <div style="width:70px;height:70px;">'
+      content +=
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '<span style="width:100%;">' +
+        생활서비스
+      content += '</span></div>'
+      content += '</div>'
       content += '    <div style="display:flex;justify-content:space-around;">'
       content += '    <div style="width:70px;height:70px;">'
       content +=
-        '        <img style="width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
         '<span style="width:100%;">' +
-        20
+        음식
+      content += '</span></div>'
+      content += '    <div style="width:70px;height:70px;">'
+      content +=
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '<span style="width:100%;">' +
+        부동산
+      content += '</span></div>'
+      content += '    <div style="width:70px;height:70px;">'
+      content +=
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '<span style="width:100%;">' +
+        의료
+      content += '</span></div>'
+      content += '    <div style="width:70px;height:70px;">'
+      content +=
+        '        <img style="margin:auto;width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
+        '<span style="width:100%;">' +
+        관광여가오락
       content += '</span></div>'
       content += '</div>'
       content += '</div>'
       return content
+    }, // 범위내 상가정보 받아오는 매서드
+    getData () {
+      axios
+        .get(
+          '/storecountByxy/' +
+            this.searchX +
+            '/' +
+            this.searchY +
+            '/' +
+            this.range
+        )
+        .then(res => {
+          var jsonlarge = res.data.large
+          this.CountInfo.소매 = jsonlarge.소매
+          this.CountInfo.학문교육 = jsonlarge.학문교육
+          this.CountInfo.숙박 = jsonlarge.숙박
+          this.CountInfo.생활서비스 = jsonlarge.생활서비스
+          this.CountInfo.음식 = jsonlarge.음식
+          this.CountInfo.부동산 = jsonlarge.부동산
+          this.CountInfo.의료 = jsonlarge.의료
+          this.CountInfo.관광여가오락 = jsonlarge.관광여가오락
+        })
+        .catch(err => alert(err, '검색어를 확인해주세요'))
     },
-    removeCircles() {
+    removeCircles () {
       for (var i = 0; i < this.circles.length; i++) {
         this.circles[i].circle.setMap(null)
         this.circles[i].polyline.setMap(null)
@@ -352,180 +478,6 @@ export default {
       }
       this.circles = []
     }
-
-    // -----------------------------------------------------------------------------------------
-    // 뱐경 그리기 함수 시작-----------------------------------------------------------------------------------------
-    // makeCircle: function() {
-    //   // if (this.$store.state.mode === 1)
-    //   let drawingFlag = false // 원이 그려지고 있는 상태를
-    //   let centerPosition // 원의 중심좌표
-    //   let drawingCircle // 그려지고 있는 원을 표시할 원 객체
-    //   let drawingLine // 그려지고 있는 원의 반지름을 표시할 선 객체
-    //   let drawingOverlay // 그려지고 있는 원의 반경을 표시할 커스텀오버레이
-
-    //   let circles = []
-
-    //   let vm = this
-    //   kakao.maps.event.addListener(this.map, 'click', function(mouseEvent) {
-    //     // 지도에 클릭 이벤트를 등록
-    //     removeCircles()
-    //     if (!vm.drawingFlag) {
-    //       // 클릭 이벤트가 발생했을 때 원을 그리고 있는 상태가 아니면 중심좌표를 클릭한 지점으로 설정
-    //       vm.drawingFlag = true
-    //       vm.centerPosition = mouseEvent.latLng // 원이 그려질 중심좌표를 클릭한 위치로 설정합니다
-    //       if (!vm.drawingLine) {
-    //         // 그려지고 있는 원의 반경을 표시할 선 객체를 생성합니다
-    //         vm.drawingLine = new kakao.maps.Polyline({
-    //           strokeWeight: 3, // 선의 두께
-    //           strokeColor: '#00a0e9', // 선의 색깔
-    //           strokeOpacity: 0, // 선의 불투명도
-    //           strokeStyle: 'solid' // 선의 스타일
-    //         })
-    //       }
-    //       if (!vm.drawingCircle) {
-    //         // 그려지고 있는 원을 표시할 원 객체를 생성합니다
-    //         vm.drawingCircle = new kakao.maps.Circle({
-    //           strokeWeight: 1,
-    //           strokeColor: '#00a0e9',
-    //           strokeOpacity: 0,
-    //           strokeStyle: 'solid',
-    //           fillColor: '#00a0e9',
-    //           fillOpacity: 0.2
-    //         })
-    //       }
-    //       if (!vm.drawingOverlay) {
-    //         // 그려지고 있는 원의 반경 정보를 표시할 커스텀오버레이를 생성합니다
-    //         vm.drawingOverlay = new kakao.maps.CustomOverlay({
-    //           xAnchor: 0,
-    //           yAnchor: 0,
-    //           zIndex: 1
-    //         })
-    //       }
-    //     }
-    //   })
-    //   kakao.maps.event.addListener(this.map, 'mousemove', function(
-    //     // 지도에 마우스무브 이벤트를 등록합니다
-    //     mouseEvent
-    //   ) {
-    //     if (vm.drawingFlag) {
-    //       // 마우스무브 이벤트가 발생했을 때 원을 그리고있는 상태이면
-    //       var mousePosition = mouseEvent.latLng // 마우스 커서의 현재 위치를 얻어옵니다
-    //       var linePath = [vm.centerPosition, mousePosition] // 그려지고 있는 선을 표시할 좌표 배열입니다. 클릭한 중심좌표와 마우스커서의 위치로 설정합니다
-    //       vm.drawingLine.setPath(linePath) // 그려지고 있는 선을 표시할 선 객체에 좌표 배열을 설정합니다
-    //       var length = vm.drawingLine.getLength() // 원의 반지름을 선 객체를 이용해서 얻어옵니다
-    //       if (length > 0) {
-    //         var circleOptions = {
-    //           // 그려지고 있는 원의 중심좌표와 반지름입니다
-    //           center: vm.centerPosition,
-    //           radius: length
-    //         }
-    //         vm.drawingCircle.setOptions(circleOptions) // 그려지고 있는 원의 옵션을 설정합니다
-    //         var radius = Math.round(vm.drawingCircle.getRadius()) // 반경 정보를 표시할 커스텀오버레이의 내용입니다
-    //         let content =
-    //           '<div class="overlay" style="background-color:white">반경 <span class="number">' +
-    //           radius +
-    //           '</span>m</div>'
-    //         vm.drawingOverlay.setPosition(mousePosition) // 반경 정보를 표시할 커스텀 오버레이의 좌표를 마우스커서 위치로 설정합니다
-    //         vm.drawingOverlay.setContent(content) // 반경 정보를 표시할 커스텀 오버레이의 표시할 내용을 설정합니다
-    //         vm.drawingCircle.setMap(vm.map) // 그려지고 있는 원을 지도에 표시합니다
-    //         vm.drawingLine.setMap(vm.map) // 그려지고 있는 선을 지도에 표시합니다
-    //         vm.drawingOverlay.setMap(vm.map) // 그려지고 있는 원의 반경정보 커스텀 오버레이를 지도에 표시합니다
-    //       } else {
-    //         vm.drawingCircle.setMap(null)
-    //         vm.drawingLine.setMap(null)
-    //         vm.drawingOverlay.setMap(null)
-    //       }
-    //     }
-    //   })
-    //   kakao.maps.event.addListener(this.map, 'rightclick', function(
-    //     // 지도에 마우스 오른쪽 클릭이벤트를 등록합니다
-    //     // 마우스 오른쪽 클릭한 위치를 기준으로 원과 원의 반경정보를 표시하는 선과 커스텀 오버레이를 표시하고 그리기를 종료합니다
-    //     mouseEvent
-    //   ) {
-    //     // alert(vm.$store.state.mode)
-    //     // vm.$store.state.mode = 0
-    //     if (vm.drawingFlag) {
-    //       var rClickPosition = mouseEvent.latLng // 마우스로 오른쪽 클릭한 위치입니다
-    //       var polyline = new kakao.maps.Polyline({
-    //         // 원의 반경을 표시할 선 객체를 생성합니다
-    //         path: [vm.centerPosition, rClickPosition], // 선을 구성하는 좌표 배열입니다. 원의 중심좌표와 클릭한 위치로 설정합니다
-    //         strokeWeight: 1, // 선의 두께 입니다
-    //         strokeColor: '#00a0e9', // 선의 색깔입니다
-    //         strokeOpacity: 0, // 선의 불투명도입니다 0에서 1 사이값이며 0에 가까울수록 투명합니다
-    //         strokeStyle: 'solid' // 선의 스타일입니다
-    //       })
-    //       var circle = new kakao.maps.Circle({
-    //         // 원 객체를 생성합니다
-    //         center: vm.centerPosition, // 원의 중심좌표입니다
-    //         radius: polyline.getLength(), // 원의 반지름입니다 m 단위 이며 선 객체를 이용해서 얻어옵니다
-    //         strokeWeight: 0, // 선의 두께입니다
-    //         strokeColor: '#00a0e9', // 선의 색깔입니다
-    //         strokeOpacity: 0, // 선의 불투명도입니다 0에서 1 사이값이며 0에 가까울수록 투명합니다
-    //         strokeStyle: 'solid', // 선의 스타일입니다
-    //         fillColor: '#00a0e9', // 채우기 색깔입니다
-    //         fillOpacity: 0.2 // 채우기 불투명도입니다
-    //       })
-    //       var radius = Math.round(circle.getRadius()) // 원의 반경 정보를 얻어옵니다
-    //       var html = getBoxHTML(radius) // 커스텀 오버레이에 표시할 반경 정보입니다
-    //       var radiusOverlay = new kakao.maps.CustomOverlay({
-    //         // 반경정보를 표시할 커스텀 오버레이를 생성합니다
-    //         content: html, // 표시할 내용입니다
-    //         position: rClickPosition, // 표시할 위치입니다. 클릭한 위치로 설정합니다
-    //         xAnchor: 0.5,
-    //         yAnchor: 1,
-    //         zIndex: 1
-    //       })
-
-    //       circle.setMap(vm.map) // 원을 지도에 표시합니다
-    //       polyline.setMap(vm.map) // 선을 지도에 표시합니다
-    //       radiusOverlay.setMap(vm.map) // 반경 정보 커스텀 오버레이를 지도에 표시합니다
-    //       let radiusObj = {
-    //         // 배열에 담을 객체입니다. 원, 선, 커스텀오버레이 객체를 가지고 있습니다
-    //         polyline: polyline,
-    //         circle: circle,
-    //         overlay: radiusOverlay
-    //       }
-    //       circles.push(radiusObj) // 이 배열을 이용해서 "모두 지우기" 버튼을 클릭했을 때 지도에 그려진 원, 선, 커스텀오버레이들을 지웁니다
-    //       vm.drawingFlag = false // 그리기 상태를 그리고 있지 않는 상태로 바꿉니다
-    //       vm.centerPosition = null // 중심 좌표를 초기화 합니다
-    //       vm.drawingCircle.setMap(null) // 그려지고 있는 원, 선, 커스텀오버레이를 지도에서 제거합니다
-    //       vm.drawingLine.setMap(null)
-    //       vm.drawingOverlay.setMap(null)
-    //     }
-    //   })
-
-    //   // 지도에 표시되어 있는 모든 원과 반경정보를 표시하는 선, 커스텀 오버레이를 지도에서 제거합니다
-    //   // eslint-disable-next-line no-unused-vars
-    //   // eslint-disable-next-line no-inner-declarations
-    //   function removeCircles() {
-    //     for (var i = 0; i < circles.length; i++) {
-    //       circles[i].circle.setMap(null)
-    //       circles[i].polyline.setMap(null)
-    //       circles[i].overlay.setMap(null)
-    //     }
-    //     circles = []
-    //   }
-
-    //   // 마우스 우클릭 하여 원 그리기가 종료됐을 때 호출하여
-    //   // HTML Content를 만들어 리턴하는 함수입니다
-    //   // eslint-disable-next-line no-inner-declarations
-    //   function getBoxHTML(distance) {
-    //     var content =
-    //       '<div class="v-sheet theme--light elevation-14" style="margin-leftauto;display:block;text-align:center;" id="mapSheet">'
-    //     content +=
-    //       '    <div style="display:flex;justify-content:space-around;">'
-    //     content += '    <div style="width:70px;height:70px;">'
-    //     content +=
-    //       '        <img style="width:40px;height:40px;display:block;" src="/img/logo.82b9c7a5.png">' +
-    //       '<span style="width:100%;">' +
-    //       20
-    //     content += '</span></div>'
-    //     content += '</div>'
-    //     content += '</div>'
-    //     return content
-    //   }
-    // }
-    // 반경 그리기 함수 끝-----------------------------------------------------------------------------------------
   }
 }
 </script>
