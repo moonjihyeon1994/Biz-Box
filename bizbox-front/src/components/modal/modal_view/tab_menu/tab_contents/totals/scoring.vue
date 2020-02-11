96281952a10e0d20b8fc1372fdd8f078a87da585
heecheol <heecheol1508@gmail.com>
2020-02-11 12:36:47 +0900

    200211 양희철 상권 성장성, 안정성 평가
    Former-commit-id: b0009c0c54c6e977f042da330e8f9b60ab7ca6cc [formerly 8c70a2f43d275717ad2f822990889804bb402279]
    Former-commit-id: 3efc7bbf964d8b840eae0e72055ca7783e43eda5

--- bizbox-front/src/components/modal/modal_view/tab_menu/tab_contents/totals/scoring.vue
+++ bizbox-front/src/components/modal/modal_view/tab_menu/tab_contents/totals/scoring.vue
@@ -189,41 +189,96 @@ export default {
           'CS300003': { 'name': '컴퓨터·주변기기', 'q1': 0, 'q2': 0, 'q3': 0, 'q4': 0, 'n1': 0, 'n2': 0, 'n3': 0, 'n4': 0 }
         }
       },
-      sales_change_rate_2018: { 'c1': 0, 'c2': 0, 'c3': 0, 'c4': 0 },
-      sales_change_rate_2017: { 'c1': 0, 'c2': 0, 'c3': 0, 'c4': 0 },
+      sales_change_2018: { 'c1': 0, 'c2': 0, 'c3': 0, 'c4': 0 },
+      sales_change_2017: { 'c1': 0, 'c2': 0, 'c3': 0, 'c4': 0 },
       growth_rate: {
         'average_rate': 0
       },
+      years: {
+        continuousYears: 0,
+        avgSeoul: 0
+      },
+      closure_rate: {
+        average: 0
+      },
       score: {
-        '성장성': '',
-        '안정성': '',
-        '영업력': '',
-        '구매력': '',
-        '집객력': ''
+        '성장성': {
+          '점수': 0,
+          '매출증감률': 0,
+          '예상성장률': 0
+        },
+        '안정성': {
+          '점수': 0,
+          '점포변동률': 0,
+          '매출변동률': 0,
+          '운영연수': 0,
+          '폐업률': 0
+        },
+        '영업력': {
+          '점수': 0,
+          '공급대비수요': 0,
+          '분기별매출편차': 0,
+          '요일별매출편차': 0
+        },
+        '구매력': {
+          '점수': 0,
+          '상권매출규모': 0,
+          '건당결제금액': 0,
+          '소비수준': 0
+        },
+        '집객력': {
+          '점수': 0,
+          '유동인구': 0,
+          '주거인구': 0,
+          '직장인구': 0
+        }
       }
     }
   },
   created () {
     // 업종 및 분기별 매출 + 점포 수
-    const requestSalesUrl = 'http://70.12.247.78:8080/sales/' + this.key
+    let vm = this
+    let requestSalesUrl = 'http://70.12.247.78:8080/sales/' + vm.key
     axios.get(requestSalesUrl)
       .then(res => {
         for (let index = 0; index < res.data.length; index++) {
           let temp = res.data[index]
-          // console.log(temp)
-          let serviceCode = temp.e
-          if (Number(temp.af) !== 0) {
-            if (temp.a === '1') {
+          let serviceCode = temp.e // 업종 코드
+
+          if (temp.a === '2018') { // 연도, 분기
+            if (temp.b === '1') {
+              this.sales_2018.q1 += Number(temp.g)
+              this.sales_2018.n1 += Number(temp.af)
+              this.sales_2018.classification[serviceCode].q1 += Number(temp.g)
+              this.sales_2018.classification[serviceCode].n1 += Number(temp.af)
+            } else if (temp.b === '2') {
+              this.sales_2018.q2 += Number(temp.g)
+              this.sales_2018.n2 += Number(temp.af)
+              this.sales_2018.classification[serviceCode].q2 += Number(temp.g)
+              this.sales_2018.classification[serviceCode].n2 += Number(temp.af)
+            } else if (temp.b === '3') {
+              this.sales_2018.q3 += Number(temp.g)
+              this.sales_2018.n3 += Number(temp.af)
+              this.sales_2018.classification[serviceCode].q3 += Number(temp.g)
+              this.sales_2018.classification[serviceCode].n3 += Number(temp.af)
+            } else {
+              this.sales_2018.q4 += Number(temp.g)
+              this.sales_2018.n4 += Number(temp.af)
+              this.sales_2018.classification[serviceCode].q4 += Number(temp.g)
+              this.sales_2018.classification[serviceCode].n4 += Number(temp.af)
+            }
+          } else if (temp.a === '2017') {
+            if (temp.b === '1') {
               this.sales_2017.q1 += Number(temp.g)
               this.sales_2017.n1 += Number(temp.af)
               this.sales_2017.classification[serviceCode].q1 += Number(temp.g)
               this.sales_2017.classification[serviceCode].n1 += Number(temp.af)
-            } else if (temp.a === '2') {
+            } else if (temp.b === '2') {
               this.sales_2017.q2 += Number(temp.g)
               this.sales_2017.n2 += Number(temp.af)
               this.sales_2017.classification[serviceCode].q2 += Number(temp.g)
               this.sales_2017.classification[serviceCode].n2 += Number(temp.af)
-            } else if (temp.a === '3') {
+            } else if (temp.b === '3') {
               this.sales_2017.q3 += Number(temp.g)
               this.sales_2017.n3 += Number(temp.af)
               this.sales_2017.classification[serviceCode].q3 += Number(temp.g)
@@ -234,51 +289,72 @@ export default {
               this.sales_2017.classification[serviceCode].q4 += Number(temp.g)
               this.sales_2017.classification[serviceCode].n4 += Number(temp.af)
             }
+          } else {
+            if (temp.b === '1') {
+              this.sales_2016.q1 += Number(temp.g)
+              this.sales_2016.n1 += Number(temp.af)
+              this.sales_2016.classification[serviceCode].q1 += Number(temp.g)
+              this.sales_2016.classification[serviceCode].n1 += Number(temp.af)
+            } else if (temp.b === '2') {
+              this.sales_2016.q2 += Number(temp.g)
+              this.sales_2016.n2 += Number(temp.af)
+              this.sales_2016.classification[serviceCode].q2 += Number(temp.g)
+              this.sales_2016.classification[serviceCode].n2 += Number(temp.af)
+            } else if (temp.b === '3') {
+              this.sales_2016.q3 += Number(temp.g)
+              this.sales_2016.n3 += Number(temp.af)
+              this.sales_2016.classification[serviceCode].q3 += Number(temp.g)
+              this.sales_2016.classification[serviceCode].n3 += Number(temp.af)
+            } else {
+              this.sales_2016.q4 += Number(temp.g)
+              this.sales_2016.n4 += Number(temp.af)
+              this.sales_2016.classification[serviceCode].q4 += Number(temp.g)
+              this.sales_2016.classification[serviceCode].n4 += Number(temp.af)
+            }
           }
-          // if (temp.a === '2018') {
-          //   if (temp.b === '1') {
-          //     this.sales_2018.q1 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].q1 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].n1 += Number(temp.af)
-          //   } else if (temp.b === '2') {
-          //     this.sales_2018.q2 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].q2 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].n2 += Number(temp.af)
-          //   } else if (temp.b === '3') {
-          //     this.sales_2018.q3 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].q3 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].n3 += Number(temp.af)
-          //   } else {
-          //     this.sales_2018.q4 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].q4 += Number(temp.g)
-          //     this.sales_2018.classification[serviceCode].n4 += Number(temp.af)
-          //   }
-          // } else if (temp.a === '2017') {
-          //   if (temp.b === '1') {
-          //     this.sales_2017.q1 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].q1 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].n1 += Number(temp.af)
-          //   } else if (temp.b === '2') {
-          //     this.sales_2017.q2 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].q2 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].n2 += Number(temp.af)
-          //   } else if (temp.b === '3') {
-          //     this.sales_2017.q3 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].q3 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].n3 += Number(temp.af)
-          //   } else {
-          //     this.sales_2017.q4 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].q4 += Number(temp.g)
-          //     this.sales_2017.classification[serviceCode].n4 += Number(temp.af)
-          //   }
-          // }
         }
+        let resquestHistoryUrl = 'http://70.12.247.78:8080/change/getHistory/' + vm.key
+        axios.get(resquestHistoryUrl).then(res => {
+          this.years.continuousYears = res.data.cblist[5].g
+          this.years.avgSeoul = res.data.cblist[5].i
+        })
+      }).then(() => {
+        let dn1 = (Math.abs(this.sales_2018.n4 - this.sales_2018.n3) / this.sales_2018.n3).toFixed(4)
+        let dn2 = (Math.abs(this.sales_2018.n3 - this.sales_2018.n2) / this.sales_2018.n2).toFixed(4)
+        let dn3 = (Math.abs(this.sales_2018.n2 - this.sales_2018.n1) / this.sales_2018.n1).toFixed(4)
+        let dn4 = (Math.abs(this.sales_2018.n1 - this.sales_2017.n4) / this.sales_2017.n4).toFixed(4)
+        let dn5 = (Math.abs(this.sales_2017.n4 - this.sales_2017.n3) / this.sales_2017.n3).toFixed(4)
+        let dn6 = (Math.abs(this.sales_2017.n3 - this.sales_2017.n2) / this.sales_2017.n2).toFixed(4)
+        let dn7 = (Math.abs(this.sales_2017.n2 - this.sales_2017.n1) / this.sales_2017.n1).toFixed(4)
+        let dn8 = (Math.abs(this.sales_2017.n1 - this.sales_2016.n4) / this.sales_2016.n4).toFixed(4)
+        this.score.안정성.점포변동률 = ((dn1 + dn2 + dn3 + dn4 + dn5 + dn6 + dn7 + dn8) / 8).toFixed(4) * 100
+        console.log('점포변동률 : ', this.score.안정성.점포변동률)
+
+        let ds1 = (Math.abs(this.sales_2018.q4 - this.sales_2018.q3) / this.sales_2018.q3).toFixed(4)
+        let ds2 = (Math.abs(this.sales_2018.q3 - this.sales_2018.q2) / this.sales_2018.q2).toFixed(4)
+        let ds3 = (Math.abs(this.sales_2018.q2 - this.sales_2018.q1) / this.sales_2018.q1).toFixed(4)
+        let ds4 = (Math.abs(this.sales_2018.q1 - this.sales_2017.q4) / this.sales_2017.q4).toFixed(4)
+        let ds5 = (Math.abs(this.sales_2017.q4 - this.sales_2017.q3) / this.sales_2017.q3).toFixed(4)
+        let ds6 = (Math.abs(this.sales_2017.q3 - this.sales_2017.q2) / this.sales_2017.q2).toFixed(4)
+        let ds7 = (Math.abs(this.sales_2017.q2 - this.sales_2017.q1) / this.sales_2017.q1).toFixed(4)
+        let ds8 = (Math.abs(this.sales_2017.q1 - this.sales_2016.q4) / this.sales_2016.q4).toFixed(4)
+        this.score.안정성.매출변동률 = ((ds1 + ds2 + ds3 + ds4 + ds5 + ds6 + ds7 + ds8) / 8).toFixed(4) * 100
+        console.log('매출변동률 : ', this.score.안정성.매출변동률)
+
+        let total2018 = this.sales_2018.q1 + this.sales_2018.q2 + this.sales_2018.q3 + this.sales_2018.q4
+        let total2017 = this.sales_2017.q1 + this.sales_2017.q2 + this.sales_2017.q3 + this.sales_2017.q4
+        let total2016 = this.sales_2016.q1 + this.sales_2016.q2 + this.sales_2016.q3 + this.sales_2016.q4
+        this.score.성장성.매출증감률 = (((total2018 - total2017) / total2017) * 10 + 5).toFixed(1)
+        console.log('매출증감률 : ', this.score.성장성.매출증감률)
+
+        this.growth_rate.average_rate = ((total2018 / total2016) ** (1 / 3) - 1).toFixed(2)
+        this.score.성장성.예상성장률 = this.growth_rate.average_rate * 10 + 3
+        console.log('예상성장률 : ', this.score.성장성.예상성장률)
       })
       .catch(err => console.log(err))
       .finally(() => {
         // 연 평균 성장률, ( + 연도끼리 비교해야함 )
-        // let total2018 = this.sales_2018.q1 + this.sales_2018.q2 + this.sales_2018.q3 + this.sales_2018.q4
-        // let total2016 = this.sales_2016.q1 + this.sales_2016.q2 + this.sales_2016.q3 + this.sales_2016.q4
+
         // this.growth_rate.average_rate = (total2018 / total2016) ** (1 / 3) - 1
         console.log(this.sales_2017.classification)
         // 매출 증감률 ( 작년 대비 )
