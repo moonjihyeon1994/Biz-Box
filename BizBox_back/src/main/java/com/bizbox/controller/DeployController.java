36195f7f196a781a0a06f93e0b793603a8232d55
Eumyounghyun <opwer032@naver.com>
2020-02-13 17:41:30 +0900

    200213 음영현 첫 배포 완료
    Former-commit-id: eada3506a300d7906e3e115c3f31e62bd1613702 [formerly 16377754c5bf028cd1e986902557854ca1912870]
    Former-commit-id: 6686b96b408208e1e8d4b78bc7c5152465f23a82

---
+++ BizBox_back/src/main/java/com/bizbox/controller/DeployController.java
@@ -0,0 +1,13 @@
+package com.bizbox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DeployController {
+
+	@RequestMapping(value="/")
+	public String main() {
+		return "index";
+	}
+}
