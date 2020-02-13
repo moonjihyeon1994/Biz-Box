1f5ac86ce7c6461f4b4b64cf26c1cb536cfd5764
문지현 <puppy613@hanmail.net>
2020-02-13 13:32:52 +0900

    200213_문지현_유저스토어추가완료
    Former-commit-id: 68960f0653d852bb2957cb75c2a687034f13f7e1 [formerly 2982623567cdacc50d652bbdf4999d5b5de2ce4a]
    Former-commit-id: b4f50b4db1f7620b7037cf6408e8b2c94facee3c

--- BizBox_back/src/main/java/com/bizbox/Service/JusoServiceImpl.java
+++ BizBox_back/src/main/java/com/bizbox/Service/JusoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bizbox.Service;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,17 +14,21 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.bizbox.apis.JusoApi;
+import com.bizbox.dao.StoreDAO;
 import com.bizbox.utils.AddressUtil;
+import com.bizbox.vo.Point;
+import com.bizbox.vo.Store;
 @Service
 public class JusoServiceImpl implements JusoService{
 	@Autowired
 	JusoApi jusoapi;
 	@Autowired
 	AddressUtil adutil;
+	@Autowired
+	StoreDAO storedao;
 	
 	/**
 	 * 도로명주소로 입력 하여 정보를 String으로 받음
@@ -366,12 +371,40 @@ public class JusoServiceImpl implements JusoService{
 	 * @throws IOException
 	 */
 	public JSONObject findAllStoreByLargeJson(String xy, String radius) throws IOException {
-		JSONArray itemsArray = jusoapi.findStore1(xy, radius);
+		String[] latlot = xy.split(",");
+		String lat = latlot[0];
+		String lot = latlot[1];
+		if(Double.parseDouble(latlot[0])>Double.parseDouble(latlot[1])) {
+			lat = latlot[1];
+			lot = latlot[0];
+		}
+		
+		String distance = String.valueOf(Double.parseDouble(radius)/1000.0);
+		Point point = new Point(lat, lot, distance);
+
+		List<Store> list = null;
+		try {
+			list = storedao.getStoreByXY(point);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
 		HashMap<String, Integer> LNm = new HashMap<String, Integer>();
+		for (int i = 0; i < list.size(); i++) {
+			String indsLclsNm = list.get(i).getCategory_large();
+			if (LNm.containsKey(indsLclsNm)) { // 대분류 당 갯수
+				LNm.put(indsLclsNm, LNm.get(indsLclsNm) + 1);
+			} else {
+				LNm.put(indsLclsNm, 1);
+			}
+		}
+		
+		JSONArray itemsArray = jusoapi.findStore1(xy, radius);
 		for (int i = 0; i < itemsArray.size(); i++) {
 			JSONObject items = (JSONObject) itemsArray.get(i);
 			String indsLclsNm = (String) items.get("indsLclsNm"); // 대분류
-			indsLclsNm = indsLclsNm.replace("/", "");
+//			indsLclsNm = indsLclsNm.replace("/", "");
 			
 			if (LNm.containsKey(indsLclsNm)) { // 대분류 당 갯수
 				LNm.put(indsLclsNm, LNm.get(indsLclsNm) + 1);
@@ -388,7 +421,6 @@ public class JusoServiceImpl implements JusoService{
 		}
 		return jsonObject;
 	}
-	
 	
 	/**
 	 * 소분류를 선택하면 범위에 해당하는 상점 이름과 주소 리턴
