b70b3104f93f0649dfaf40d44c268f04a1cb0261
heecheol <heecheol1508@gmail.com>
2020-02-14 17:37:50 +0900

    200214 양희철 그래프 데이터 수정
    Former-commit-id: 477f5dbde5d47e7de7bd8d553239bc59c8dff1b3 [formerly eae2acfb0388d72e85ca15dfa18b5a3fd467d4a0]
    Former-commit-id: 1e263707e02caca4aac8d13df8040011c731ef36

--- bizbox-front/src/js/http-commons.js
+++ bizbox-front/src/js/http-commons.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
   // baseURL: 'http://localhost:8080',
   // baseURL: 'local://13.125.20.125:8081',
-  baseURL: 'http://70.12.246.137:8080',
+  baseURL: 'http://70.12.246.137:8080'
   // baseURL: 'http://70.12.247.78:8080',
   // headers: {
   //   'Content-type': 'application/json'
