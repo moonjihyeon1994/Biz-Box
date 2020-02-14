ea368b1dc9b188917aa2614a0bf1b5685bb1949e
osw <tmddhks4717@naver.com>
2020-02-14 16:15:02 +0900

    200214
    Former-commit-id: 131e60f6e054264ca4149f98d92522f063552d33 [formerly 78cde0a7805d5300e6d4112bee8230d1077dc387]
    Former-commit-id: 5dd490051e35bf6548be5d459eb22dfbd4141ae2

--- BizBox_back/src/main/java/com/bizbox/Service/PredictionService.java
+++ BizBox_back/src/main/java/com/bizbox/Service/PredictionService.java
@@ -1,9 +1,11 @@
 package com.bizbox.Service;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 import org.json.simple.JSONObject;
 
 public interface PredictionService {
 	JSONObject findAllPrediction(String cx, String cy) throws Exception;
+	JSONObject findBusinessAnalysis(String cx, String cy, String business) throws IOException;
 }
