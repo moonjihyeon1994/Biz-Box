5f2f3d2835359782e1b333bf34018ee348e3e6c1
JaehyunKim94 <didwjd169@gmail.com>
2020-02-17 19:59:54 +0900

    200217_김재현_mypage chart init
    Former-commit-id: b417c5209ca93d492d80d65d6e10295c0fb56017 [formerly 5dc4f50a8649f0bd4e3b9f110eeb6b4009a11af3]
    Former-commit-id: e8c25aeb36873d08bf035d727b7cf23ffa897047

--- bizbox-front/src/components/mypage/bizreport/BizReport.vue
+++ bizbox-front/src/components/mypage/bizreport/BizReport.vue
@@ -3,12 +3,17 @@
     <div class="map-pic">
       <div id="my-pos"></div>
       <h3>Where at(위치를 카카오맵으로 표시)</h3>
-      위도: {{ content.lat }}
-      경도: {{ content.lng }}
+      위도: {{ content.lat }} 경도: {{ content.lng }}
     </div>
     <div class="biz-info">
       <h3>INFO (가게에 대한 정보)</h3>
       개업일: {{ content.open }}
+      <line-chart
+        :chart-data="chartdata"
+        :options="chartoptions"
+        width="500px"
+        height="300px"
+      ></line-chart>
     </div>
     <div class="btn-set">
       <button class="btn-item">정보 수정</button>
@@ -18,27 +23,27 @@
 </template>
 
 <script>
+import LineChart from '@/lib/LineChart'
 export default {
-  data() {
+  data () {
     return {
-      mapContainer: null
+      mapContainer: null,
+      labels: [
+        '19년 9월',
+        '19년 10월',
+        '19년 11월',
+        '19년 12월',
+        '20년 1월',
+        '20년 2월'
+      ],
+      data: [4839023, 5838293, 4937194, 3937869, 4928493, 30458283]
     }
   },
   props: {
     content: Object
   },
-  methods: {
-    getMap: function() {
-      var container = document.getElementById('mypos')
-      var mapOptions = {
-        center: new kakao.map.LatLng(this.content.lat, this.content.lng),
-        level: 3
-      }
-      this.mapContainer = new kakao.maps.Map(container, mapOptions)
-    }
-  },
-  mounted() {
-    this.getMap()
+  components: {
+    LineChart
   }
 }
 </script>
