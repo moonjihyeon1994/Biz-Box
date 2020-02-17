c36497d8be90c9d3264d750c43c7e44d2b49aaec
문요한 <munyohan@naver.com>
2020-02-17 14:48:32 +0900

    200217_문요한_Modal컴포넌트 밑에 Chart 컴포넌트 작성
    Former-commit-id: 8e7bbc9a747e9c76fe94057dff575de1a376da99 [formerly e7dc676a41aec91c0bcc4789cb6ad94a7e4183c0]
    Former-commit-id: cf1de73bd6bdcae5dd8f73d6d455b5d7ff642a11

--- bizbox-front/src/components/modal/modal_view/tab_menu/tab_contents/chart/Chart.vue
+++ bizbox-front/src/components/modal/modal_view/tab_menu/tab_contents/chart/Chart.vue
@@ -1,32 +1,67 @@
 <template>
   <div id='bz-container'>
-    <div id='title'>업종별 차트</div>
+    <div id='title'>상세 차트</div>
 
     <div id='chart-box'>
       <div id='chart-box-flex-container'>
         <div class='select-box-wrapper'>
           <p>대분류</p>
-          <select-box />
+          <select-box :list='listL' @large-event='parentsMethodL' />
         </div>
         <div class='select-box-wrapper'>
           <p>중분류</p>
-          <select-box />
+          <select-box :list='listR' :init='isInit' @mid-event='parentsMethodM'/>
         </div>
       </div>
 
       <div class='bz-each-title'>매출 차트</div>
+      <div>
+        <line-chart chartWidth='500px' chartHeight='200px' :large='large' :mid='mid' />
+      </div>
 
       <div class='bz-each-title'>창업/폐업 현황</div>
+
+      <div class='bz-each-title'>원클릭 컨설팅</div>
     </div>
   </div>
 </template>
 
 <script>
 import SelectBox from './selectbox/SelectBox'
+import ListJson from '@/assets/json/Select.json'
+import LineChart from './linechart/LineChart'
 
 export default {
   components: {
-    SelectBox
+    SelectBox,
+    LineChart
+  },
+  data () {
+    return {
+      listL: ListJson.large,
+      listR: [],
+      isInit: false,
+      large: '전체',
+      mid: '전체'
+    }
+  },
+  methods: {
+    parentsMethodL (selected) {
+      const array = ListJson.mid
+      for (let index = 0; index < array.length; index++) {
+        if (array[index].name === selected) {
+          this.listR = array[index].data
+          this.isInit = true
+          this.large = selected
+          return
+        }
+      }
+      this.large = '전체'
+      this.listR = []
+    },
+    parentsMethodM (selected) {
+      this.mid = selected
+    }
   }
 }
 </script>
