1778aa750b1cb677860c869b0234ea690e3b504c
JaehyunKim94 <didwjd169@gmail.com>
2020-02-18 10:26:30 +0900

    200218_김재현_mypage chart added
    Former-commit-id: bffb3995caba067f8afb7b8b7cda3cf64b61003d [formerly 2abfcfa0d8ef81736d58c2d3be2f73dfb7d6bf16]
    Former-commit-id: b3ab3a0bbbb522e1e460030f99ace11954586236

--- bizbox-front/src/components/mypage/bizreport/BizReport.vue
+++ bizbox-front/src/components/mypage/bizreport/BizReport.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="biz-report">
-    <div class="map-pic">
-      <div id="my-pos"></div>
-      <h3>Where at(위치를 카카오맵으로 표시)</h3>
-      위도: {{ content.lat }} 경도: {{ content.lng }}
-    </div>
     <div class="biz-info">
       <h3>INFO (가게에 대한 정보)</h3>
       개업일: {{ content.open }}
-      <line-chart
+      <bar-chart
         :chart-data="chartdata"
         :options="chartoptions"
-        width="500px"
-        height="300px"
-      ></line-chart>
+        :height="gheight"
+      ></bar-chart>
     </div>
     <div class="btn-set">
       <button class="btn-item">정보 수정</button>
@@ -23,11 +17,13 @@
 </template>
 
 <script>
-import LineChart from '@/lib/LineChart'
+import BarChart from '@/lib/BarChart'
 export default {
   data () {
     return {
-      mapContainer: null,
+      gheight: 300,
+      chartdata: null,
+      chartoptions: null,
       labels: [
         '19년 9월',
         '19년 10월',
@@ -36,14 +32,104 @@ export default {
         '20년 1월',
         '20년 2월'
       ],
-      data: [4839023, 5838293, 4937194, 3937869, 4928493, 30458283]
+      title: '종합 보고서',
+      compmoney: [4839023, 5838293, 4937194, 3937869, 4928493, 3458283],
+      building: [2300000, 2300000, 2300000, 2500000, 2500000, 2500000],
+      getmoney: [14532532, 13532923, 15293852, 14704932, 15028342, 14920394],
+      mymoney: [0, 0, 0, 0, 0, 0],
+      point: 0,
+      btnStyle1: {
+        backgroundColor: '#d9d9d9',
+        cursor: 'pointer'
+      },
+      btnStyle2: {
+        backgroundColor: 'white',
+        cursor: 'pointer'
+      },
+      btnStyle3: {
+        backgroundColor: 'white',
+        cursor: 'pointer'
+      },
+      btnStyle4: {
+        backgroundColor: 'white',
+        cursor: 'pointer'
+      },
+      loadingStatus: false,
+      allowDiv: {
+        display: 'none'
+      }
     }
   },
   props: {
     content: Object
   },
   components: {
-    LineChart
+    BarChart
+  },
+  methods: {
+    calcMoney () {
+      for (let idx = 0; idx < this.labels.length; idx++) {
+        this.mymoney[idx] =
+          this.getmoney[idx] - this.building[idx] - this.compmoney[idx]
+      }
+      this.chartdata = {
+        labels: this.labels,
+        datasets: [
+          {
+            label: '순이익',
+            backgroundColor: 'lightgreen',
+            data: this.mymoney
+          },
+          {
+            label: '매출액',
+            backgroundColor: '#74ddf7',
+            data: this.getmoney
+          },
+          {
+            label: '비용',
+            backgroundColor: '#ff6390',
+            data: this.compmoney
+          },
+          {
+            label: '건물 임대료',
+            backgroundColor: '#365673',
+            data: this.building
+          }
+        ]
+      }
+      this.chartoptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: true
+              }
+            }
+          ]
+        }
+      }
+      this.loadingStatus = false
+      this.allowDiv.display = 'none'
+      this.btnStyle1.cursor = 'pointer'
+      this.btnStyle2.cursor = 'pointer'
+      this.btnStyle3.cursor = 'pointer'
+      this.btnStyle4.cursor = 'pointer'
+    }
+  },
+  mounted () {
+    this.calcMoney()
   }
 }
 </script>
