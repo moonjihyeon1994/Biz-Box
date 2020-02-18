8530665c680bdf75ce17c9db721e8cf3337cd441
문요한 <munyohan@naver.com>
2020-02-18 10:36:47 +0900

    200218_문요한_Total컴포넌트 무한로딩문제 해결. 단 고정데이터 유동적으로 바꾸어야 함.
    Former-commit-id: 03b089e1eb57742f3bfeb6db4dc95876337f7323 [formerly dbe2ab6877ffaa14c9409ecfa0249593cf1091ba]
    Former-commit-id: 6d79ed81debd29e80af03afaac751f4280551d16

--- bizbox-front/src/js/http-commons.js
+++ bizbox-front/src/js/http-commons.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default axios.create({
   // baseURL: 'http://localhost:8080',
-  // baseURL: 'local://13.125.20.125:8081',
-  baseURL: 'http://70.12.246.137:8080'
+  // baseURL: 'local://13.125.20.125:80'
+  baseURL: 'http://70.12.246.137:80'
   // baseURL: 'http://70.12.247.78:8080',
   // headers: {
   //   'Content-type': 'application/json'
