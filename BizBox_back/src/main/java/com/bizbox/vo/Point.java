2f527084657a59380feb2d03666a05461d374ddb
문지현 <puppy613@hanmail.net>
2020-02-18 19:13:00 +0900

    200218_문지현_원검색수정
    Former-commit-id: c64eef1b275bc5becd105a2457c9235faf3867ea [formerly e87351bbf58df820605b8af180fe8921108ca32b]
    Former-commit-id: 2dbdad3ff8d50c7dabfefcf2034ec8c09159e370

--- BizBox_back/src/main/java/com/bizbox/vo/Point.java
+++ BizBox_back/src/main/java/com/bizbox/vo/Point.java
@@ -10,5 +10,12 @@ import lombok.NoArgsConstructor;
 public class Point {
 	private String lat;
 	private String lot;
+	private String Oplat;
+	private String Oplot;
 	private String distance;
+	public Point(String lat, String lot, String distance) {
+		this.lat = lat;
+		this.lot = lot;
+		this.distance = distance;
+	}
 }
