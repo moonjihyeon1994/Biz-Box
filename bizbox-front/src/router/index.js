55ed8697a619cddccada5b92a9a978bc0ef75662
osw <tmddhks4717@naver.com>
2020-02-18 17:08:48 +0900

    -
    Former-commit-id: c8e4c56409ec3259c6978822f307d70b54f4bbe0 [formerly 088c0b9a9e0a7762f47d9b2ef063e270ca7427a8]
    Former-commit-id: d5936853f393f7887d5a0926c7bd8975a886c414

--- bizbox-front/src/router/index.js
+++ bizbox-front/src/router/index.js
@@ -4,9 +4,7 @@ import Home from '@/components/home/Home.vue'
 // import About from '@/components/about/About.vue'
 // import Result from '@/components/result/Result.vue'
 // import ResultSearch from '@/components/result/ResultSearch.vue'
-import map from '@/components/map/Newmap.vue'
 import Map from '@/components/bizmap/kakaomap/KakaoMap.vue'
-import BizMap from '@/components/bizmap/BizMap'
 import Modal from '@/components/modal/Modal.vue'
 import tap from '@/components/countstore/CountStores.vue'
 import result from '@/components/modal/result/Result.vue'
@@ -25,19 +23,9 @@ const routes = [
   //   component: ResultSearch
   // },
   {
-    path: '/map',
-    name: 'newResult',
-    component: map
-  },
-  {
-    path: '/eummap',
-    name: 'mapcd .',
-    component: Map
-  },
-  {
     path: '/bizmap',
     name: 'bizmap',
-    component: BizMap
+    component: Map
   },
   {
     path: '/modal',
