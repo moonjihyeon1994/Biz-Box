e3a0badd5187f1bde81f15e8f8f2ad52cea5580a
moonjihyeon1994 <52726655+moonjihyeon1994@users.noreply.github.com>
2020-05-13 15:03:34 +0900

    Update NaverApi.java

--- BizBox_back/src/main/java/com/bizbox/apis/NaverApi.java
+++ BizBox_back/src/main/java/com/bizbox/apis/NaverApi.java
@@ -11,8 +11,7 @@ import org.springframework.stereotype.Service;
 public class NaverApi {
 
     public String na(){
-        String clientId = "DirkGh0LQiWoZy1cuWa6";//애플리케이션 클라이언트 아이디값";
-        String clientSecret = "NJuL9qMmxf";//애플리케이션 클라이언트 시크릿값";
+        
         try {
             String apiURL = "https://openapi.naver.com/v1/datalab/search";
             String body = "{\"startDate\":\"2017-01-01\",\"endDate\":\"2017-04-30\",\"timeUnit\":\"month\",\"keywordGroups\":[{\"groupName\":\"한글\",\"keywords\":[\"한글\",\"korean\"]},{\"groupName\":\"영어\",\"keywords\":[\"영어\",\"english\"]}],\"device\":\"pc\",\"ages\":[\"1\",\"2\"],\"gender\":\"f\"}";
